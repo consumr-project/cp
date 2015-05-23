14b820d08a0ddf13ea29b90e2d0b5081b4dfe335
Marcos Minond <minond.marcos@gmail.com>
2015-05-22 21:29:18 -0600

    array notation on config

--- public/js/routes.js
+++ public/js/routes.js
@@ -1,10 +1,10 @@
 'use strict';
 
-angular.module('tcp').config(function ($routeProvider, $locationProvider) {
+angular.module('tcp').config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
     $routeProvider.when('/search', {
         templateUrl: '/public/views/search.html',
         controller: 'SearchController'
     });
 
     $locationProvider.html5Mode(true);
-});
+}]);
