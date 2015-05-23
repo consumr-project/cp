3bbc03762c2b80f7a21bf3f5155c051e6e8c950f
Marcos Minond <minond.marcos@gmail.com>
2015-05-22 19:50:00 -0600

    serving index

--- app.js
+++ app.js
@@ -5,13 +5,26 @@ var DEBUG = process.env.NODE_ENV === 'development';
 var express = require('express'),
     serve_index = require('serve-index'),
     not_found = require('not-found'),
-    error_handler = require('errorhandler');
+    error_handler = require('errorhandler'),
+    swig = require('swig');
 
 var app = express();
 
+app.set('view cache', true);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/public/views');
+app.engine('html', swig.renderFile);
+
 app.use('/public', express.static('public'));
-if (DEBUG) app.use('/public', serve_index('public'));
+app.use('/node_modules', express.static('node_modules'));
+app.get('/', function (req, res) { res.render('index'); });
 app.use(not_found(app.get('views') + '404.html'));
-if (DEBUG) app.use(error_handler());
+
+if (DEBUG) {
+    app.use('/public', serve_index('public'));
+    app.use(error_handler());
+    app.set('view cache', false);
+    swig.setDefaults({ cache: false });
+}
 
 app.listen(process.env.PORT || 3000);
