a9d415fb574341e865dabdd044b830805469d412
Marcos Minond <minond.marcos@gmail.com>
2015-05-25 16:34:27 -0600

    loading indirator

--- public/js/SearchController.js
+++ public/js/SearchController.js
@@ -1,8 +1,11 @@
 'use strict';
 
 angular.module('tcp').controller('SearchController', ['$scope', function ($scope) {
+    $scope.searching = false;
+
     $scope.search = function (text) {
         // TODO
+        $scope.searching = true;
         console.log('searching for %s', text);
     };
 }]);
