3415360c4533f35ff7a69b83caea01dd3622bb7c
Marcos Minond <minond.marcos@gmail.com>
2015-05-25 15:20:21 -0600

    adding submit handler

--- public/js/SearchController.js
+++ public/js/SearchController.js
@@ -1,4 +1,8 @@
 'use strict';
 
 angular.module('tcp').controller('SearchController', ['$scope', function ($scope) {
+    $scope.search = function (text) {
+        // TODO
+        console.log('searching for %s', text);
+    };
 }]);
