90b0aee7b323eb130fcceaa2ad9d71a063697331
Marcos Minond <marcos.minond@domo.com>
2015-05-27 22:58:43 -0600

    fetch articles

--- app/modules/extract/extract.js
+++ app/modules/extract/extract.js
@@ -2,12 +2,23 @@
 
 angular.module('extract', []).service('extract', ['$http', function ($http) {
     /**
+     * @param {String} object
+     * @return {Function}
+     */
+    function pluck(object) {
+        return function (res) {
+            return res.data[object];
+        };
+    }
+
+    /**
      * extract a page's content
      * @param {String} url
      * @return {Promise<Object>}
      */
     function fetch(url) {
-        return $http.get('/extract?url=' + encodeURIComponent(url));
+        return $http.get('/extract?url=' + encodeURIComponent(url))
+            .then(pluck('response'));
     }
 
     return {
--- app/modules/post/postController.js
+++ app/modules/post/postController.js
@@ -1,9 +1,22 @@
-angular.module('tcp').controller('postController', ['postService', '$scope', function (postService, $scope) {
-  $scope.postIt = function () {
-    postService.add({
-      company: $scope.postCompany,
-      link: $scope.postLink,
-      tags: $scope.postTags,
-    });
-  };
+angular.module('tcp').controller('postController', ['$scope', 'postService', 'extract', function ($scope, postService, extract) {
+    $scope.editing = true;
+
+    // XXX - remove once done testing
+    $scope.url = 'http://www.nytimes.com/2015/05/28/world/asia/chinas-high-hopes-for-growing-those-rubber-tree-plants.html';
+
+    $scope.fetchArticle = function () {
+        if ($scope.url) {
+            extract.fetch($scope.url).then(function (article) {
+                $scope.article = article;
+            });
+        }
+    };
+
+    $scope.edit = function () {
+        $scope.editing = true;
+    };
+
+    $scope.save = function () {
+        $scope.editing = false;
+    };
 }]);
