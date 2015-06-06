5a81615696e5fce13e8959959eeca8bb64fcf6fe
Marcos Minond <minond.marcos@gmail.com>
2015-06-06 17:10:44 -0600

    google service

--- app/modules/base/main.js
+++ app/modules/base/main.js
@@ -1,4 +1,4 @@
-/* global Firebase, wikipedia, extract, _, highlighter */
+/* global Firebase, wikipedia, extract, _, highlighter, google */
 (function () {
     'use strict';
 
@@ -9,6 +9,7 @@
     angular.module('tcp').value('extract', extract);
     angular.module('tcp').value('lodash', _);
     angular.module('tcp').value('highlighter', highlighter);
+    angular.module('tcp').value('google', google);
     angular.module('tcp').value('store', store);
     angular.module('tcp').value('postStore', store.child('post'));
     angular.module('tcp').value('userStore', store.child('user'));
