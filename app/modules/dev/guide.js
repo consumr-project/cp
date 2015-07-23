9954295c08e57809fe1597b58321ca0e9ec2d722
Marcos Minond <minond.marcos@gmail.com>
2015-07-22 22:27:11 -0600

    guide toc

--- app/modules/dev/guide.js
+++ app/modules/dev/guide.js
@@ -20,3 +20,28 @@ angular.module('tcp').controller('guideController', [
             $scope.addTag();
     }
 ]);
+
+angular.module('tcp').directive('toc', function () {
+    return {
+        template: '<div class="toc--items"></div>',
+        link: function (scope, elem) {
+            var $items = elem.find('.toc--items'),
+                $body = $('html, body');
+
+            $('[guide-section]').each(function () {
+                var $this = $(this),
+                    label = $this.text();
+
+                $('<div></div>')
+                    .text(label)
+                    .appendTo($items)
+                    .addClass('toc--item')
+                    .on('click', function () {
+                        $body.animate({
+                            scrollTop: $this.offset().top - 30
+                        }, 700, 'swing');
+                    });
+            });
+        }
+    };
+});
