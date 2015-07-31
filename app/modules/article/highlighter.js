69a83b48c94aedb5545ab1fea803093f888ef00b
Marcos Minond <minond.marcos@gmail.com>
2015-07-31 12:57:50 -0600

    highlighter cleanup

--- app/modules/article/highlighter.js
+++ app/modules/article/highlighter.js
@@ -8,31 +8,37 @@ angular.module('tcp').directive('highlighter', [
 
         return {
             scope: {
-                highlighterOnClick: '&',
                 highlighterOnHighlight: '&',
                 highlighterOnHighlightClick: '&',
+                highlighterClassName: '@'
             },
-            link: function (scope, elem, attr) {
-                var name = attr.highlighterClassName || DEFAULT_HIGHLIGHT_CLASS_NAME,
-                    pen = highlighter.create(name),
+            link: function (scope, elem) {
+                var class_name = scope.highlighterClassName || DEFAULT_HIGHLIGHT_CLASS_NAME,
+                    pen = highlighter.create(class_name),
                     id = elem.attr('id');
 
                 if (!id) {
                     console.warn('Missing `id` on', elem);
                 }
 
-                function handleElementClick(ev) {
-                    scope.highlighterOnClick({
+                /**
+                 * passed to highlighter event handlers
+                 * @param {Event} ev
+                 * @param {Highlight} [highlight]
+                 * @return {Object}
+                 */
+                function eventPackage(ev, highlight) {
+                    return {
                         $event: ev,
+                        highlight: highlight || pen.getHighlightForElement(ev.target),
+                        pen: pen,
                         target: ev.target
-                    });
+                    };
+                }
 
-                    if (angular.element(ev.target).hasClass(name)) {
-                        scope.highlighterOnHighlightClick({
-                            $event: ev,
-                            highlight: pen.getHighlightForElement(ev.target),
-                            target: ev.target
-                        });
+                function handleElementClick(ev) {
+                    if (angular.element(ev.target).hasClass(class_name)) {
+                        scope.highlighterOnHighlightClick(eventPackage(ev));
                     }
                 }
 
@@ -42,22 +48,17 @@ angular.module('tcp').directive('highlighter', [
                     });
 
                     if (highlights.length) {
-                        scope.highlighterOnHighlight({
-                            $event: ev,
-                            highlight: highlights[0],
-                            highlights: highlights,
-                            pen: pen,
-                            target: ev.target
-                        });
+                        scope.highlighterOnHighlight(eventPackage(ev, highlights[0]));
                     }
+                }
+
+                function unhandleDocumentMouseUp() {
+                    $document.off('mouseup', handleDocumentMouseUp);
                 }
 
                 elem.on('click', handleElementClick);
                 $document.on('mouseup', handleDocumentMouseUp);
-
-                scope.$on('$destroy', function () {
-                    $document.off('mouseup', handleDocumentMouseUp);
-                });
+                scope.$on('$destroy', unhandleDocumentMouseUp);
             }
         };
     }
