4a6996a58af19f69af80bed34db4e070733bbba9
Marcos Minond <minond.marcos@gmail.com>
2015-08-02 16:22:00 -0600

    useful indicator alignment fixed

--- app/elements/indicator/indicator.js
+++ app/elements/indicator/indicator.js
@@ -2,17 +2,13 @@ angular.module('tcp').directive('indicator', function () {
     'use strict';
 
     return {
-        template: '{{value}}',
+        template: '<span class="indicator-useful--value-{{value}}">{{value}}</span>',
         scope: {
             value: '='
         },
         link: function (scope, elem, attrs) {
             elem.attr('tabindex', '0');
             elem.addClass('is-non-selectable indicator-' + attrs.type);
-
-            if (attrs.type === 'useful' && scope.value === 0) {
-                scope.value = '';
-            }
         }
     };
 });
