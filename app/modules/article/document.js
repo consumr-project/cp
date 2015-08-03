5e546ed79403a2d2c7948c8a90a5a71be517c918
Marcos Minond <minond.marcos@gmail.com>
2015-08-02 23:04:01 -0600

    document directive

---
+++ app/modules/article/document.js
@@ -0,0 +1,12 @@
+angular.module('tcp').directive('document', function () {
+    'use strict';
+
+    return {
+        replace: true,
+        templateUrl: '/app/modules/article/document.html',
+        scope: {
+            info: '=',
+            onFoundUseful: '&'
+        }
+    };
+});
