5552e767b35c7b555c4f32644c8b90e2889de97f
Marcos Minond <minond.marcos@gmail.com>
2015-08-25 23:13:26 -0600

    login menu

--- app/modules/admin/adminController.js
+++ app/modules/admin/adminController.js
@@ -4,24 +4,40 @@ angular.module('tcp').controller('adminController', [
     function ($scope, Auth) {
         'use strict';
 
-        $scope.settings = {
-            show: false,
-            login: function () {
-                $scope.settings.show = false;
-                $scope.loginPopover.show();
-            }
+        $scope.state = {
+            loggedIn: false
         };
 
         $scope.loginPopover = {
+            showLoginScreen: false,
+            showMoreOptions: false,
+
             // from popover api
             hide: null,
-            show: null,
-
-            showMoreOptions: false,
-            linkedinLogin: function () {
-                $scope.loginPopover.hide();
-                return Auth.login(Auth.PROVIDER.LINKEDIN);
-            }
+            show: null
         };
+
+        $scope.linkedinLogin = function () {
+            $scope.loginPopover.hide();
+            return Auth.login(Auth.PROVIDER.LINKEDIN);
+        };
+
+        $scope.login = function () {
+            $scope.loginPopover.showLoginScreen = false;
+            $scope.loginPopover.show();
+        };
+
+        $scope.logout = function () {
+            $scope.loginPopover.showLoginScreen = false;
+            Auth.logout();
+        };
+
+        Auth.on('login', function () {
+            $scope.state.loggedIn = true;
+        });
+
+        Auth.on('logout', function () {
+            $scope.state.loggedIn = false;
+        });
     }
 ]);
--- app/services/auth.js
+++ app/services/auth.js
@@ -4,10 +4,21 @@ angular.module('tcp').service('Auth', [
     'CONFIG',
     'logger',
     'store',
-    function (DEBUGGING, CONFIG, logger, store) {
+    'lodash',
+    function (DEBUGGING, CONFIG, logger, store, _) {
         'use strict';
 
-        var log = logger('auth');
+        var Auth, auth;
+
+        var log = logger('auth'),
+            events = {};
+
+        var EVENT = {
+            LOGIN: 'login',
+            LOGOUT: 'logout',
+            TIMEOUT: 'timeout',
+            ERROR: 'error'
+        };
 
         var PROVIDER = {
             LINKEDIN: 'linkedin'
@@ -17,32 +28,71 @@ angular.module('tcp').service('Auth', [
             EXPIRED_TOKEN: 'EXPIRED_TOKEN'
         };
 
-        var auth = new FirebasePassportLogin(store, function (err, user) {
+        Auth = {
+            PROVIDER: PROVIDER,
+            login: login,
+            logout: logout,
+            on: addListener
+        };
+
+        auth = new FirebasePassportLogin(store, function (err, user) {
             if (err && err.code !== ERROR.EXPIRED_TOKEN) {
                 log.error('login error', err);
                 Auth.USER = null;
+                triggerListeners(EVENT.ERROR);
             } else if (err) {
                 log('session timeout');
                 Auth.USER = null;
+                triggerListeners(EVENT.TIMEOUT);
             } else if (user) {
                 log('user login', user);
                 Auth.USER = user;
+                triggerListeners(EVENT.LOGIN);
             } else {
                 log('user logout');
                 Auth.USER = null;
+                triggerListeners(EVENT.LOGOUT);
             }
         }, CONFIG.auth.url);
 
-        var Auth = {
-            PROVIDER: PROVIDER,
-            login: login
-        };
+        /**
+         * @param {String} name
+         * @param {Array} args
+         */
+        function triggerListeners(name, args) {
+            _.each(events[name], function (fn) {
+                fn.apply(null, args || []);
+            });
+        }
+
+        /**
+         * @param {String} name
+         * @param {Function} handler
+         * @return {Function} remove listener
+         */
+        function addListener(name, handler) {
+            if (!(name in events)) {
+                events[name] = [];
+            }
+
+            events[name].push(handler);
+
+            return function () {
+                _.remove(events[name], function (fn) {
+                    return handler === fn;
+                });
+            };
+        }
 
         /**
          * @param {String} provider
          */
         function login(provider) {
             return auth.login(provider);
+        }
+
+        function logout() {
+            return auth.logout();
         }
 
         if (DEBUGGING) {
