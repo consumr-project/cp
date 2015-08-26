87841453c808c7022f85ef15577e8d29e159ff88
Marcos Minond <minond.marcos@gmail.com>
2015-08-25 22:53:45 -0600

    setting popup

--- app/modules/admin/adminController.js
+++ app/modules/admin/adminController.js
@@ -4,7 +4,19 @@ angular.module('tcp').controller('adminController', [
     function ($scope, Auth) {
         'use strict';
 
+        $scope.settings = {
+            show: false,
+            login: function () {
+                $scope.settings.show = false;
+                $scope.loginPopover.show();
+            }
+        };
+
         $scope.loginPopover = {
+            // from popover api
+            hide: null,
+            show: null,
+
             showMoreOptions: false,
             linkedinLogin: function () {
                 $scope.loginPopover.hide();
