74a6a59e443b8697cecce0492618574daaa7a7d1
Marcos Minond <minond.marcos@gmail.com>
2015-08-31 22:45:32 -0600

    configurable date format

--- app/elements/i18n/i18n.js
+++ app/elements/i18n/i18n.js
@@ -1,21 +1,38 @@
 angular.module('tcp').directive('i18n', [
+    'CONFIG',
     'i18n',
     'moment',
-    function (i18n, moment) {
+    function (CONFIG, i18n, moment) {
         'use strict';
+
+        /**
+         * @param {String} [data]
+         * @return {Object}
+         */
+        function getData(data) {
+            /* jshint evil: true */
+            return eval(['(', data || '{}', ')'].join(''));
+        }
+
+        /**
+         * @param {String} date
+         * @return {Number|String}
+         */
+        function getDate(date) {
+            return isFinite(date) ? +date : date;
+        }
 
         return {
             link: function (scope, elem, attrs) {
-                var key = attrs.i18n || attrs.str,
-                    data = attrs.data || '{}',
-                    date = attrs.date,
-                    format = attrs.format || 'LL';
+                var format = attrs.format || CONFIG.locate.dateFormat,
+                    key = attrs.i18n || attrs.str,
+                    data = attrs.data,
+                    date = attrs.date;
 
                 if (key) {
-                    /* jshint evil: true */
-                    elem.text(i18n.get(key, eval(['(', data, ')'].join(''))));
+                    elem.text(i18n.get(key, getData(data)));
                 } else if (date) {
-                    elem.text(moment(isFinite(date) ? +date : date).format(format));
+                    elem.text(moment(getDate(date)).format(format));
                 }
             }
         };
