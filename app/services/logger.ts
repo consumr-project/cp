6b3e728e119fc7d20ed8438b48525192dab70e4d
Marcos Minond <minond.marcos@gmail.com>
2015-09-17 22:05:29 -0600

    tracking new logger file

---
+++ app/services/logger.ts
@@ -0,0 +1,41 @@
+import {newNoop} from './utils';
+
+interface LogFunction {
+    (...args: Array<any>): void;
+}
+
+interface Logger {
+    <LogFunction>();
+    error<LogFunction>();
+    info<LogFunction>();
+    log<LogFunction>();
+    warn<LogFunction>();
+}
+
+function label(name: string): string {
+    return `[${name}]`;
+}
+
+function timestamp(): string {
+    return `(${(new Date()).toJSON()})`;
+}
+
+function logFn(name: string, fn: string): LogFunction {
+    return function (...args: Array<any>): void {
+        args[0] = `${label(name)} ${args[0]} ${timestamp()}`;
+        console[fn].apply(console, args);
+    };
+}
+
+export default function factory(enabled: Boolean = true): (name: string) => Logger {
+    return function (name: string): Logger {
+        var log = <Logger>(enabled ? logFn(name, 'info') : newNoop());
+
+        log.error = <LogFunction>(enabled ? logFn(name, 'error') : newNoop());
+        log.info = <LogFunction>(enabled ? logFn(name, 'info') : newNoop());
+        log.log = <LogFunction>(enabled ? logFn(name, 'log') : newNoop());
+        log.warn = <LogFunction>(enabled ? logFn(name, 'warn') : newNoop());
+
+        return log;
+    };
+};
