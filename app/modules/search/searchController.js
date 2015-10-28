2e68edc87c79d03fd98a2f8ed4ea91438ad12c4e
Marcos Minond <minond.marcos@gmail.com>
2015-10-28 00:29:57 -0600

    search results

--- app/modules/search/searchController.js
+++ app/modules/search/searchController.js
@@ -1,11 +1,67 @@
+// XXX move out of this file
+angular.module('tcp').service('SearchService', [
+    '$q',
+    'store',
+    'lodash',
+    function ($q, store, lodash) {
+        var COMPANY = { _type: 'company' };
+
+        return {
+            entity: entity,
+            search: search
+        };
+
+        /**
+         * @param {String} query
+         * @return {Promise}
+         */
+        function entity(query) {
+            return search('entity', '', query);
+        };
+
+        /**
+         * @param {String} index
+         * @param {String} type
+         * @param {String} query
+         * @return {Promise}
+         */
+        function search(index, type, query) {
+            return $q(function (resolve, reject) {
+                var job = store.child('search').push({
+                    index: index,
+                    type: type,
+                    query: query
+                });
+
+                store.child('search').child(job.key()).on('value', function handler(res) {
+                    var results = res.val();
+
+                    if (lodash.isString(results)) {
+                        res.ref().off('value', handler);
+                        res.ref().remove();
+
+                        results = JSON.parse(results);
+                        results.$all = results.hits.hits;
+                        results.$companies = lodash.where(results.hits.hits, COMPANY);
+
+                        resolve(results);
+                    }
+                });
+            });
+        }
+    }
+]);
+
 angular.module('tcp').controller('searchController', [
     '$scope',
     '$routeParams',
     'NavigationService',
-    function ($scope, $routeParams, NavigationService) {
+    'SearchService',
+    function ($scope, $routeParams, NavigationService, SearchService) {
         'use strict';
 
         $scope.query = $routeParams.q;
+        $scope.results = {};
 
         /**
          * @param {String} query
@@ -16,7 +72,14 @@ angular.module('tcp').controller('searchController', [
                 query = ev.target.elements.q.value;
             }
 
+            $scope.loading = true;
+            $scope.results = {};
+
             NavigationService.search(query);
+            SearchService.entity(query).then(function (res) {
+                $scope.loading = false;
+                $scope.results.company = res.$companies;
+            });
 
             if (ev) {
                 ev.preventDefault();
