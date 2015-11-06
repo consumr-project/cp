85dda7b1437ea5338a0ac0395b8615d7a32ac8b6
Marcos Minond <minond.marcos@gmail.com>
2015-11-05 22:51:21 -0700

    separating company-event content out of company html

---
+++ app/elements/company-event/company-event.js
@@ -0,0 +1,8 @@
+angular.module('tcp').directive('companyEvent', function () {
+    'use strict';
+
+    return {
+        replace: true,
+        templateUrl: '/app/elements/company-event/company-event.html'
+    };
+});
