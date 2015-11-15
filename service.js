c774389b8c63db82e3e6ca270a5ffe678f1a37cd
Marcos Minond <minond.marcos@gmail.com>
2015-11-14 23:22:41 -0700

    templates

--- service.js
+++ service.js
@@ -11,7 +11,13 @@ var config = require('acm'),
     debug = require('debug'),
     log = debug('service');
 
-var i18n = require('./build/i18n');
+var i18n = require('./build/i18n'),
+    tmpl = require('lodash/string/template'),
+    read = require('fs').readFileSync;
+
+var templates = {
+    welcome: tmpl(read('./templates/welcome.tmpl'))
+};
 
 firebase = new Firebase(config('firebase.url'));
 email = Transport({
@@ -26,8 +32,8 @@ email.sendMail({
     from: config('email.addresses.do_not_reply'),
     to: config('email.service.user'),
     subject: i18n.en.get('common/welcome_email_subject'),
-    text: i18n.en.get('common/welcome_email_text', {name: 'Marcos'}),
-    html: i18n.en.get('common/welcome_email_text', {name: 'Marcos'})
+    text: templates.welcome({ user: { name: 'Marcos' }, i18n: i18n.en }),
+    html: templates.welcome({ user: { name: 'Marcos' }, i18n: i18n.en })
 }, function (err, info) {
     console.log(err);
     console.log(info);
