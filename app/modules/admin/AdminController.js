703401712ba7f63eac76e29284ba92d78545bddd
Marcos Minond <minond.marcos@gmail.com>
2015-11-22 13:53:54 -0700

    code clean up in User controller

--- app/modules/admin/AdminController.js
+++ app/modules/admin/AdminController.js
@@ -8,14 +8,15 @@ angular.module('tcp').controller('AdminController', [
     function ($scope, Auth, utils, users, Cookie, _) {
         'use strict';
 
-        var COOKIE_SESSION = 'client:session';
+        // active session. cached in `client:session` cookie
+        $scope.session = getSession();
 
-        $scope.session = Cookie.getJSON(COOKIE_SESSION) || {};
-
+        // actions menu under avatar in header
         $scope.actions = {
             show: null
         };
 
+        // login popover settings
         $scope.login = {
             more: false,
 
@@ -24,33 +25,56 @@ angular.module('tcp').controller('AdminController', [
             show: null
         };
 
-        $scope.withLinkedin = function () {
+        $scope.withLinkedin = loginWithLinkedin;
+        $scope.login = login;
+        $scope.logout = logout;
+
+        $scope.$watchCollection('session', cacheSession);
+        Auth.on(Auth.EVENT.LOGIN, fetchCurrentUser);
+        Auth.on(Auth.EVENT.LOGOUT, clearSession);
+
+        function loginWithLinkedin() {
             $scope.login.hide();
             return Auth.login(Auth.PROVIDER.LINKEDIN);
-        };
+        }
 
-        $scope.login = function () {
+        function login() {
             $scope.login.show();
-        };
+        }
 
-        $scope.logout = function () {
+        function logout() {
             Auth.logout();
             $scope.actions.show = false;
-        };
+        }
 
-        $scope.$watchCollection('session', _.curry(Cookie.set, 2)(COOKIE_SESSION));
-
-        Auth.on(Auth.EVENT.LOGIN, function () {
-            users.get(Auth.USER.uid).then(function (user) {
-              _.extend(Auth.USER, user);
-              $scope.session.userAvatarUrl = user.avatarUrl;
-              $scope.session.loggedIn = true;
-              $scope.$apply();
+        /**
+         * updates Auth.USER with additional information from store
+         */
+        function fetchCurrentUser() {
+            return users.get(Auth.USER.uid).then(function (user) {
+                _.extend(Auth.USER, user);
+                $scope.session.userAvatarUrl = user.avatarUrl;
+                $scope.session.loggedIn = true;
+                $scope.$apply();
             });
-        });
+        }
 
-        Auth.on(Auth.EVENT.LOGOUT, function () {
+        function clearSession() {
             $scope.session = {};
-        });
+        }
+
+        /**
+         * @param {Object} session (see $scope.session)
+         */
+        function cacheSession(session) {
+            return Cookie.set('client:session', session);
+        }
+
+        /**
+         * @return {Object}
+         */
+        function getSession() {
+            return Cookie.getJSON('client:session') || {};
+        }
     }
 ]);
