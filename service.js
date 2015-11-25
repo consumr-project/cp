b6d8b4606dcbc7ea514c64fc2c13e9a01bdce204
Marcos Minond <minond.marcos@gmail.com>
2015-11-25 15:09:48 -0700

    removing firebase tied searching

--- service.js
+++ service.js
@@ -11,8 +11,7 @@ var config = require('acm'),
     debug = require('debug'),
     log = debug('service');
 
-var indexer = require('./src/indexer'),
-    searcher = require('./src/searcher');
+var indexer = require('./src/indexer');
 
 log('firebase application %s', config('firebase.url'));
 log('elasticsearch host %s', config('elasticsearch.host'));
@@ -20,7 +19,6 @@ log('elasticsearch host %s', config('elasticsearch.host'));
 firebase = new Firebase(config('firebase.url'));
 elasticsearch = new Elasticsearch({ host: config('elasticsearch.host') });
 
-log('starting indexer and searcher jobs');
+log('starting indexer job');
 indexer(elasticsearch, firebase, 'user', ['fullName']);
 indexer(elasticsearch, firebase, 'company', ['name', 'summary']);
-searcher(elasticsearch, firebase);
--- src/searcher.js
+++ src/searcher.js
@@ -14,17 +14,11 @@
  */
 var config = require('acm');
 
-var debug = require('debug'),
-    log = debug('searcher'),
-    error = debug('searcher:error');
-
-var query_ttl = config('firebase.query_ttl');
-
 /**
  * @param {String} str
- * @return {Object}
+ * @return {Query}
  */
-function query(str) {
+function fuzzyQuery(str) {
     return {
         query: {
             fuzzy_like_this: {
@@ -32,52 +26,6 @@ function query(str) {
                 like_text: str
             }
         }
-    };
-}
-
-/**
- * @param {Firebase} ref
- * @param {String} key
- * @return {Function(value: Object)}
- */
-function updateReference(ref, key) {
-    return function (value) {
-        log('responding to %s', key);
-        ref.child(key).set(JSON.stringify(value));
-    };
-}
-
-/**
- * @param {Object} search
- * @param {String} key
- * @return {Function(Error)}
- */
-function logErrored(search, key) {
-    return function (err) {
-        error('errored on %s {index: %s, type: %s, query: %s}: [%s]',
-            key, search.index, search.type, search.query, err);
-    };
-}
-
-/**
- * @param {ElasticsearchClient} elasticsearch
- * @param {Firebase} firebase
- * @return {Function(ref: Firebase)}
- */
-function runSearch(elasticsearch, firebase) {
-    return function (ref) {
-        var val = ref.val(),
-            key = ref.key();
-
-        log('querying %s', key);
-        elasticsearch.search({
-            index: val.index,
-            type: val.type,
-            body: query(val.query)
-        }).then(updateReference(firebase, key), logErrored(val, key));
-
-        log('cleaning up in %sms', query_ttl);
-        setTimeout(ref.ref().remove.bind(ref.ref()), query_ttl);
     };
 }
 
@@ -92,7 +40,7 @@ function fuzzySearch(elasticsearch, search) {
         from: search.from,
         index: search.index,
         type: search.type,
-        body: query(search.query)
+        body: fuzzyQuery(search.query)
     });
 }
 
@@ -110,17 +58,6 @@ function handleRequest(elasticsearch, searchFn, req, res, next) {
             res.json(results);
         });
 }
-
-/**
- * @param {ElasticsearchClient} elasticsearch
- * @param {Firebase} ref
- * @return {Firebase}
- */
-module.exports = function (elasticsearch, firebase) {
-    var ref = firebase.child(config('firebase.search_collection'));
-    ref.on('child_added', runSearch(elasticsearch, ref));
-    return ref;
-};
 
 module.exports.fuzzySearch = fuzzySearch;
 module.exports.handleRequest = handleRequest;
