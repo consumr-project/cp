fc2099bfd9dcc4aeb6c9aca67a9a0c30b4233d7c
Marcos Minond <minond.marcos@gmail.com>
2015-12-13 20:22:34 -0700

    fixing company event tag relationships

--- db/models/company_event_tag.js
+++ db/models/company_event_tag.js
@@ -3,7 +3,8 @@
 var u = require('../../src/utils');
 
 module.exports = function (sequelize, DataTypes) {
-    var CompanyEvent = require('./company_event')(sequelize, DataTypes);
+    var CompanyEvent = require('./company_event')(sequelize, DataTypes),
+        Tag = require('./tag')(sequelize, DataTypes);
 
     var CompanyEventTag = sequelize.define('company_event_tag', u.merge({
         company_event_id: {
@@ -17,7 +18,8 @@ module.exports = function (sequelize, DataTypes) {
         }
     }), u.configuration());
 
-    CompanyEventTag.belongsTo(CompanyEvent);
+    CompanyEvent.belongsToMany(Tag, { through: CompanyEventTag });
+    Tag.belongsToMany(CompanyEvent, { through: CompanyEventTag });
 
     return CompanyEventTag;
 };
