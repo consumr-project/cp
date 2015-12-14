c235e0e7174c007bfdd1d99da9211846a0489ea9
Marcos Minond <minond.marcos@gmail.com>
2015-12-13 21:18:31 -0700

    removing company event tags relationship definition from company event model

--- db/models/company_event.js
+++ db/models/company_event.js
@@ -3,8 +3,7 @@
 var u = require('../../src/utils');
 
 module.exports = function (sequelize, DataTypes) {
-    var Tag = require('./tag')(sequelize, DataTypes),
-        Company = require('./company')(sequelize, DataTypes);
+    var Company = require('./company')(sequelize, DataTypes);
 
     var CompanyEvent = sequelize.define('company_event', u.merge(u.doneBy(DataTypes), {
         id: {
@@ -24,8 +23,6 @@ module.exports = function (sequelize, DataTypes) {
     }), u.configuration());
 
     CompanyEvent.belongsTo(Company);
-    CompanyEvent.belongsToMany(Tag, { through: 'company_event_tags' });
-    Tag.belongsToMany(CompanyEvent, { through: 'company_event_tags' });
 
     return CompanyEvent;
 };
