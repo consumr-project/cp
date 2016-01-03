cbbb32d83fb451a3876198e0050cfb99ea1d9c06
Marcos Minond <minond.marcos@gmail.com>
2016-01-02 18:34:30 -0700

    fixing indentation

--- app/services/NavigationService.js
+++ app/services/NavigationService.js
@@ -1,61 +1,64 @@
-angular.module('tcp').service('NavigationService', ['$location', function ($location) {
-    'use strict';
+angular.module('tcp').service('NavigationService', [
+    '$location',
+    function ($location) {
+        'use strict';
 
-    var BASES = {
-        COMPANY: '/company',
-        HOME: '/',
-        SEARCH: '/search',
-        USER: '/user'
-    };
+        var BASES = {
+            COMPANY: '/company',
+            HOME: '/',
+            SEARCH: '/search',
+            USER: '/user'
+        };
 
-    /**
-     * optional append
-     * @param {String} [next] part
-     * @return {String}
-     */
-    function oappend(next) {
-        return next ? '/' + next : '';
-    }
+        /**
+         * optional append
+         * @param {String} [next] part
+         * @return {String}
+         */
+        function oappend(next) {
+            return next ? '/' + next : '';
+        }
 
-    /**
-     * generate a $location.url funciton
-     * @param {String} base
-     * @return {Function(String id?)}
-     */
-    function withoid(base) {
-        return function (id) {
-            $location.url(base + oappend(id));
+        /**
+         * generate a $location.url funciton
+         * @param {String} base
+         * @return {Function(String id?)}
+         */
+        function withoid(base) {
+            return function (id) {
+                $location.url(base + oappend(id));
+            };
+        }
+
+        return {
+            BASES: BASES,
+
+            /**
+             * reroute functions:
+             * @param {String} [id]
+             */
+            company: withoid(BASES.COMPANY),
+            home: withoid(BASES.HOME),
+            user: withoid(BASES.USER),
+
+            /**
+             * go to search
+             * @param {String} query
+             */
+            search: function (query) {
+                $location.url('/search')
+                    .search({ q: query });
+            },
+
+            /**
+             * are you in one of these pages?
+             * @param {String[]} pages
+             * @return {Boolean}
+             */
+            oneOf: function (pages) {
+                var path = $location.path();
+                return pages.indexOf(path) !== -1;
+            }
         };
     }
-
-    return {
-        BASES: BASES,
-
-        /**
-         * reroute functions:
-         * @param {String} [id]
-         */
-        company: withoid(BASES.COMPANY),
-        home: withoid(BASES.HOME),
-        user: withoid(BASES.USER),
-
-        /**
-         * go to search
-         * @param {String} query
-         */
-        search: function (query) {
-            $location.url('/search')
-                .search({ q: query });
-        },
-
-        /**
-         * are you in one of these pages?
-         * @param {String[]} pages
-         * @return {Boolean}
-         */
-        oneOf: function (pages) {
-            var path = $location.path();
-            return pages.indexOf(path) !== -1;
-        }
-    };
-}]);
+]);
