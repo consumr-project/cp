12fa94d540d382bc1277fd1cd8fa19d6a863315d
Marcos Minond <minond.marcos@gmail.com>
2016-01-21 20:16:39 -0700

    fixing some user page issues

--- app/components/user.js
+++ app/components/user.js
@@ -16,7 +16,7 @@ angular.module('tcp').directive('user', [
             function load(id) {
                 return ServicesService.query.users.retrieve(id).then(function (user) {
                     $scope.user = user;
-                    $scope.user.$summary = utils.summaryze(user.summary);
+                    $scope.user.$summary = utils.summaryze(user.summary || '');
                     $scope.user.$followers_count = 0;
                     $scope.user.$following_count = 0;
                 });
@@ -30,8 +30,19 @@ angular.module('tcp').directive('user', [
                 $scope.vm.myself = $scope.id === SessionService.USER.id;
             }
 
-            $scope.onStartFollowing = function () {
+            /**
+             * @return {void}
+             */
+            $scope.on_start_following = function () {
                 $scope.user.$followers_count++;
+            };
+
+            /**
+             * @param {User} user
+             * @return {Boolean}
+             */
+            $scope.is_elsewhere = function (user) {
+                return !!user.linkedin_url;
             };
 
             if ($scope.id) {
@@ -57,7 +68,7 @@ angular.module('tcp').directive('user', [
                 '          image="{{::user.avatar_url}}"></avatar>',
 
                 '        <div class="block">',
-                '            <button ng-click="onStartFollowing()"',
+                '            <button ng-click="on_start_following()"',
                 '              ng-invisible="vm.myself || !vm.loggedin"',
                 '              class="margin-top-xlarge margin-bottom-xlarge"',
                 '              i18n="admin/follow"></button>',
@@ -67,14 +78,14 @@ angular.module('tcp').directive('user', [
                 '            <tr>',
                 '                <th i18n="user/following"></th>',
                 '                <th i18n="user/followers"></th>',
-                '                <th i18n="user/elsewhere"></th>',
+                '                <th i18n="user/elsewhere" ng-if="::is_elsewhere(user)"></th>',
                 '            </tr>',
                 '            <tr>',
                 '                <td ng-if="!user.$following_count" i18n="common/none"></td>',
                 '                <td ng-if="user.$following_count">{{user.$followers_count}}</td>',
                 '                <td ng-if="!user.$followers_count" i18n="common/none"></td>',
                 '                <td ng-if="user.$followers_count">{{user.$followers_count}}</td>',
-                '                <td>',
+                '                <td ng-if="::is_elsewhere(user)">',
                 '                    <a ng-if="::user.linkedin_url" href="{{::user.linkedin_url}}" target="_blank">',
                 '                        <img alt="" src="/assets/images/linkedin.png" style="height: 20px; width: 20px" />',
                 '                    </a>',
