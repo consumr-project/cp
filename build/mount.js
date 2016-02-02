15ddac1641b605ea356a3df06be75f8b32cba48b
Marcos Minond <minond.marcos@gmail.com>
2016-02-01 22:17:12 -0700

    default export is mountable app

--- build/mount.js
+++ build/mount.js
@@ -4,11 +4,10 @@ var config = require('acm');
 var crunchbase_1 = require('./crunchbase');
 var wiki_1 = require('./wiki');
 var page_1 = require('./page');
-exports.app = express();
-exports.app.get('/page', page_1.extract);
-exports.app.get('/crunchbase/companies', crunchbase_1.companies);
-exports.app.get('/wiki/search', wiki_1.search);
-exports.app.get('/wiki/extract', wiki_1.extract);
-if (!module.parent) {
-    exports.app.listen(config('port') || 3000);
-}
+var app = express();
+app.get('/page', page_1.extract);
+app.get('/crunchbase/companies', crunchbase_1.companies);
+app.get('/wiki/search', wiki_1.search);
+app.get('/wiki/extract', wiki_1.extract);
+!module.parent && app.listen(config('port') || 3000);
+module.exports = app;
--- src/mount.ts
+++ src/mount.ts
@@ -5,13 +5,12 @@ import { companies as crunchbase_companies } from './crunchbase';
 import { search as wiki_search, extract as wiki_extract } from './wiki';
 import { extract as page_extract } from './page';
 
-export var app = express();
+var app = express();
 
 app.get('/page', page_extract);
 app.get('/crunchbase/companies', crunchbase_companies);
 app.get('/wiki/search', wiki_search);
 app.get('/wiki/extract', wiki_extract);
 
-if (!module.parent) {
-    app.listen(config('port') || 3000);
-}
+!module.parent && app.listen(config('port') || 3000);
+export = app;
