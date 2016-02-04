d25374c33eef496fc0a4466d5094718b1aef7e56
Marcos Minond <marcos.minond@domo.com>
2016-02-03 21:37:22 -0700

    addition tweaks to search query

--- config/search.sql
+++ config/search.sql
@@ -1,21 +1,21 @@
-select u.id, u.name, substring(u.summary from 0 for 200), 'user' as type
+select u.id, u.name, substring(u.summary from 0 for 300), 'user' as type
 from users u
 where u.deleted_date is null
 and name like :q
 
 union
-select c.id, c.name, substring(c.summary from 0 for 200), 'company' as type
+select c.id, c.name, substring(c.summary from 0 for 300), 'company' as type
 from companies c
 where c.deleted_date is null
 and name like :q
 
--- union
--- select t.id, t."en-US" as name, '' as summary, 'tag' as type
--- from tags t
--- where t.deleted_date is null
--- and t.approved
--- and t."en-US" like :q
+union
+select t.id, t."en-US" as name, '' as summary, 'tag' as type
+from tags t
+where t.deleted_date is null
+and t.approved
+and t."en-US" like :q
 
 order by type
-limit 100
+limit :limit
 offset :offset
