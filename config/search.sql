250a383bbf0f2ee6c32f492ab29bb8b97c024a27
Marcos Minond <marcos.minond@domo.com>
2016-02-03 21:43:25 -0700

    case insensitive search

--- config/search.sql
+++ config/search.sql
@@ -1,26 +1,26 @@
 select u.id, u.name, substring(u.summary from 0 for 300), 'user' as type
 from users u
 where u.deleted_date is null
-and name like :q
+and name ilike :q
 
 union
 select c.id, c.name, substring(c.summary from 0 for 300), 'company' as type
 from companies c
 where c.deleted_date is null
-and name like :q
+and name ilike :q
 
 union
 select e.id, e.title as name, '' as summary, 'event' as type
 from events e
 where e.deleted_date is null
-and title like :q
+and title ilike :q
 
 union
 select t.id, t."en-US" as name, '' as summary, 'tag' as type
 from tags t
 where t.deleted_date is null
 and t.approved
-and t."en-US" like :q
+and t."en-US" ilike :q
 
 order by type
 limit :limit
