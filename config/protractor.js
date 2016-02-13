4d685e6eea9e6e2c50910a7366c863fabcfaa1f1
Marcos Minond <minond.marcos@gmail.com>
2016-02-12 22:55:10 -0700

    renaming utils to init

--- config/protractor.js
+++ config/protractor.js
@@ -1,7 +1,7 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: [
-        '../test/e2e/utils.js',
+        '../test/e2e/init.js',
         '../test/e2e/*-spec.js'
     ],
 
