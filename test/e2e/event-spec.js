c36cc25fb6c66e42b46f387e8d77be69f76184dd
Marcos Minond <minond.marcos@gmail.com>
2016-02-14 19:18:44 -0700

    event spec

---
+++ test/e2e/event-spec.js
@@ -0,0 +1,57 @@
+describe('event', () => {
+    /* global $, navigation */
+    'use strict';
+
+    beforeEach(() => {
+        navigation.company('walmart');
+        $('[i18n="event/add"]').click();
+    });
+
+    it('automatically gets article information', () => {
+        $('#source_1_url')
+            .sendKeys('http://www.bbc.com/news/election-us-2016-35572261');
+
+        expect($('#event_title').getAttribute('value'))
+            .toEqual('Donald Trump and Jeb Bush go head to head in debate - BBC News');
+
+        expect($('#event_date').getAttribute('value'))
+            .toEqual('2016-02-13');
+
+        expect($('#event_source_0_source').getAttribute('value'))
+            .toEqual('http://www.bbc.com/news/election-us-2016-35572261');
+
+        expect($('#event_source_0_title').getAttribute('value'))
+            .toEqual('Donald Trump and Jeb Bush go head to head in debate - BBC News');
+
+        expect($('#event_source_0_date').getAttribute('value'))
+            .toEqual('2016-02-13');
+    });
+
+    it('clears everything upon cancellation', () => {
+        $('#source_1_url')
+            .sendKeys('http://www.bbc.com/news/election-us-2016-35572261');
+
+        $('.event-elem [i18n="admin/cancel"]')
+            .click();
+
+        expect($('#event_title').getAttribute('value'))
+            .toEqual('');
+    });
+
+    it('can add multiple sources', () => {
+        $('.event-elem [i18n="event/add_source"]')
+            .click();
+
+        $('#source_1_url')
+            .sendKeys('http://www.bbc.com/news/election-us-2016-35572261');
+
+        $('#event_source_1_source')
+            .sendKeys('http://www.bbc.com/news/magazine-35559589');
+
+        expect($('#event_source_1_title').getAttribute('value'))
+            .toEqual('The first \'friendly fire\' victim of World War One - BBC News');
+
+        expect($('#event_source_1_date').getAttribute('value'))
+            .toEqual('2016-02-14');
+    });
+});
