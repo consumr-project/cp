424c0d697f661144c1323232a745360bbab5d2d5
Marcos Minond <marcos.minond@domo.com>
2016-02-27 12:06:47 -0700

    tracking new typings

--- config/typings.d.ts
+++ config/typings.d.ts
@@ -5,6 +5,11 @@ declare module "acm" {
     export = fn;
 }
 
+interface CPServiceResponseV1<T> {
+    body: T | Array<T> | { [index: string]: T };
+    meta?: any;
+}
+
 declare module "node-uuid" {
     interface UUIDOptions {
 
