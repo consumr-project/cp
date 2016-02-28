d3b7b5eab22d355ff066241a02584e8d52cab7a7
Marcos Minond <marcos.minond@domo.com>
2016-02-28 02:40:05 -0700

    testing companies

--- test/integration/query.js
+++ test/integration/query.js
@@ -1,13 +1,45 @@
 'use strict';
 
-const test = require('tape');
+const config = require('acm');
+const test = require('tapes');
+const clone = require('lodash').clone;
+
 const get = require('./utils').get;
+const post = require('./utils').post;
+const put = require('./utils').put;
+const del = require('./utils').del;
+const purge = require('./utils').purge;
+const patch = require('./utils').patch;
+
+const user = require('./utils').user;
+const login = require('./utils').login;
+const logout = require('./utils').logout;
+
+const fixture = clone(config('fixtures.integration_tests'));
 
 test('query', t => {
-    t.plan(2);
+    t.plan(1);
 
-    get('/query/users/e68a1d8e-256e-4dcd-b111-f7dd0e7f63e4').end((err, res) => {
-        t.ok(res.body.meta.ok, 'successful query');
-        t.equals(res.body.body.name, 'Zaphod Beeblebrox');
+    login(fixture.user.admin).end((err, res) => {
+        t.error(err);
+
+        fixture.company.created_by = res.body.id;
+        fixture.company.updated_by = res.body.id;
+    });
+
+    t.test('setup', st => {
+        st.plan(1);
+
+        purge('/companies/' + fixture.company.id).end((err, res) => {
+            st.ok(res.body.meta.ok, 'deleted test company');
+        });
+    });
+
+    t.test('create company', st => {
+        st.plan(1);
+
+        post('/companies', fixture.company).end((err, res) => {
+            st.ok(res.body.meta.ok, 'can create a company');
+        });
     });
 });
