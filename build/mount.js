11e73fa0e926858f16af0ee5e53f183ad6c371f5
Marcos Minond <marcos.minond@domo.com>
2016-03-03 20:02:20 -0700

    completing mount

--- build/mount.js
+++ build/mount.js
@@ -5,6 +5,7 @@ var express = require('express');
 var models_1 = require('./models');
 var routes_1 = require('./routes');
 var Sequelize = require('sequelize');
+var auth, cookie, session;
 var config = require('acm'), log = debug('service:query');
 var app = express();
 module.exports = exports = app;
@@ -14,4 +15,18 @@ exports.conn = new Sequelize(config('database.url'), {
 });
 exports.models = models_1["default"](exports.conn);
 app.use(body.json());
+if (!module.parent) {
+    auth = require('auth-service');
+    cookie = require('cookie-parser');
+    session = require('express-session');
+    app.use(cookie('session.secret'));
+    app.use(session({ secret: 'session.secret' }));
+    app.use(auth.passport.initialize());
+    app.use(auth.passport.session());
+    app.use(auth.as_guest);
+    app.use('/auth', auth);
+}
 routes_1["default"](app, exports.models);
+if (!module.parent) {
+    app.listen(config('port') || 3000);
+}
--- src/mount.ts
+++ src/mount.ts
@@ -8,6 +8,8 @@ import gen_routes from './routes';
 
 import Sequelize = require('sequelize');
 
+var auth, cookie, session;
+
 var config = require('acm'),
     log = debug('service:query');
 
@@ -22,4 +24,23 @@ export var conn = new Sequelize(config('database.url'), {
 export var models = gen_models(conn);
 
 app.use(body.json());
+
+if (!module.parent) {
+    auth = require('auth-service');
+    cookie = require('cookie-parser');
+    session = require('express-session');
+
+    app.use(cookie('session.secret'));
+    app.use(session({ secret: 'session.secret' }));
+
+    app.use(auth.passport.initialize());
+    app.use(auth.passport.session());
+    app.use(auth.as_guest);
+    app.use('/auth', auth);
+}
+
 gen_routes(app, models);
+
+if (!module.parent) {
+    app.listen(config('port') || 3000);
+}
