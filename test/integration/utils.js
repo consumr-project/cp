f9933cad5fd43afc53770d4cfc100bfe5690e3d0
Marcos Minond <marcos.minond@domo.com>
2016-03-03 21:53:42 -0700

    using new service url env var name

--- test/integration/utils.js
+++ test/integration/utils.js
@@ -1,22 +1,22 @@
 'use strict';
 
-const SERVICE_URL = process.env.SERVICE_URL;
+const TEST_SERVICE_URL = process.env.TEST_SERVICE_URL;
 
 const test = require('tape');
 const superagent = require('superagent');
 
 test('setup', t => {
     t.plan(1);
-    t.ok(SERVICE_URL, 'service url is set (process.env.SERVICE_URL)');
-    t.comment(`service url: ${SERVICE_URL}`);
+    t.ok(TEST_SERVICE_URL, 'service url is set (process.env.TEST_SERVICE_URL)');
+    t.comment(`service url: ${TEST_SERVICE_URL}`);
 
-    if (!SERVICE_URL) {
+    if (!TEST_SERVICE_URL) {
         process.exit(1);
     }
 });
 
 module.exports = {
     get(path) {
-        return superagent.get(SERVICE_URL + path);
+        return superagent.get(TEST_SERVICE_URL + path);
     }
 };
