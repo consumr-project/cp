05fee646f3178ea18921550ef44e2e40dbf724ed
Marcos Minond <marcos.minond@domo.com>
2016-03-10 23:11:17 -0700

    completing tests

--- test/integration/common-tags.js
+++ test/integration/common-tags.js
@@ -1,18 +1,15 @@
 'use strict';
 
+// XXX this test is sometimes failing
+
 const cp = require('base-service/test/utils');
 const clone = require('lodash').clone;
 const config = require('acm');
 const fixture = clone(config('fixtures'));
 
 // XXX fix in base-service
-cp.patch = (path, data) => {
-    // console.log('==================================');
-    // console.log(path);
-    // console.log(JSON.stringify(data));
-    // console.log('==================================');
-    return cp.agent.patch(process.env.TEST_SERVICE_URL + path).send(data);
-};
+cp.patch = (path, data) =>
+    cp.agent.patch(process.env.TEST_SERVICE_URL + path).send(data);
 
 // for (var i = 0; i < 10; i++)
 cp.tapes('common tag', t => {
@@ -62,9 +59,8 @@ cp.tapes('common tag', t => {
             fixture.tags.length * 2 +
 
             // linking
-            // 12 * 2 +
-            // fixture.events.length * 2
-            0
+            12 * 2 +
+            fixture.events.length * 2
         );
 
         cp.post('/companies', fixture.company).end((err, res) => {
@@ -84,53 +80,57 @@ cp.tapes('common tag', t => {
                 st.ok(res.body.meta.ok, `created test tag (${tag.id})`);
             }));
 
-        // fixture.events.forEach(ev =>
-        //     cp.patch(`/companies/${fixture.company.id}/events`, {
-        //         event_id: ev.id
-        //     }).end((err, res) => {
-        //         if (err) {
-        //             console.log('=========================')
-        //             console.log(err)
-        //             console.log('=========================')
-        //             process.exit();
-        //         }
-        //         st.error(err, 'no error');
-        //         st.ok(res.body.meta.ok, `linking company to event (${ev.id})`);
-        //     }));
+        fixture.events.forEach((ev, i) => {
+            setTimeout(() => {
+                cp.patch(`/companies/${fixture.company.id}/events`, {
+                    event_id: ev.id
+                }).end((err, res) => {
+                    st.error(err, 'no error');
+                    st.ok(res.body.meta.ok, `linking company to event (${ev.id})`);
+                });
+            }, 100 * i);
+        });
 
-        // [
-        //     [ fixture.events[0], [ fixture.tags[0], fixture.tags[1], fixture.tags[2] ] ],
-        //     [ fixture.events[1], [ fixture.tags[0], fixture.tags[1], fixture.tags[2] ] ],
-        //     [ fixture.events[2], [ fixture.tags[1], fixture.tags[3], fixture.tags[4] ] ],
-        //     [ fixture.events[3], [ fixture.tags[1], fixture.tags[5], fixture.tags[2] ] ],
-        // ].forEach(entities => {
-        //     var ev = entities[0],
-        //         tags = entities[1];
-        //
-        //     tags.forEach(tag => {
-        //         cp.patch(`/events/${ev.id}/tags`, {
-        //             tag_id: tag.id
-        //         }).end((err, res) => {
-        //             st.error(err, 'no error');
-        //             st.ok(res.body.meta.ok, `linking event to tag (${tag.id})`);
-        //         });
-        //     });
-        // });
+        [
+            [ fixture.events[0], [ fixture.tags[0], fixture.tags[1], fixture.tags[2] ] ],
+            [ fixture.events[1], [ fixture.tags[0], fixture.tags[1], fixture.tags[2] ] ],
+            [ fixture.events[2], [ fixture.tags[1], fixture.tags[3], fixture.tags[4] ] ],
+            [ fixture.events[3], [ fixture.tags[1], fixture.tags[5], fixture.tags[2] ] ],
+        ].forEach(entities => {
+            var ev = entities[0],
+                tags = entities[1];
+
+            tags.forEach((tag, i) => {
+                setTimeout(() => {
+                    cp.patch(`/events/${ev.id}/tags`, {
+                        tag_id: tag.id
+                    }).end((err, res) => {
+                        st.error(err, 'no error');
+                        st.ok(res.body.meta.ok, `linking event to tag (${tag.id})`);
+                    });
+                }, 100 * i);
+            });
+        });
     });
 
-    // t.test('create event', st => {
-    //     st.plan(2);
-    //
-    //     cp.get(`/companies/${fixture.company.id}/events/tags/common`).end((err, res) => {
-    //         var tags = fixture.tags;
-    //         var common = res.body.body;
-    //
-    //         st.error(err);
-    //         st.ok(res.body.meta.ok, 'can create an event');
-    //
-    //         // st.equal(common[0].id, tags[1].id);
-    //     });
-    // });
+    t.test('create event', st => {
+        st.plan(8);
+
+        cp.get(`/companies/${fixture.company.id}/events/tags/common`).end((err, res) => {
+            var tags = fixture.tags;
+            var common = res.body.body;
+
+            st.error(err);
+            st.ok(res.body.meta.ok, 'can create an event');
+
+            st.equal(common[0].id, tags[1].id);
+            st.equal(common[1].id, tags[2].id);
+            st.equal(common[2].id, tags[0].id);
+            st.equal(common[3].id, tags[4].id);
+            st.equal(common[4].id, tags[5].id);
+            st.equal(common[5].id, tags[3].id);
+        });
+    });
 
     t.test('reset', st => {
         st.plan(
@@ -139,15 +139,24 @@ cp.tapes('common tag', t => {
             fixture.tags.length
         );
 
-        cp.purge(`/companies/${fixture.company.id}`).end((err, res) =>
-            st.ok(res.body.meta.ok, 'deleted test company'));
+        cp.purge(`/companies/${fixture.company.id}`).end((err, res) => {
+            st.ok(res.body.meta.ok, 'deleted test company');
 
-        fixture.events.forEach(ev =>
-            cp.purge(`/events/${ev.id}`).end((err, res) =>
-                st.ok(res.body.meta.ok, `deleted test event (${ev.id})`)));
+            fixture.events.forEach((ev, i) => {
+                setTimeout(() => {
+                    cp.purge(`/events/${ev.id}`).end((err, res) =>
+                        st.ok(res.body.meta.ok, `deleted test event (${ev.id})`));
+                }, 100 * i);
+            });
 
-        fixture.tags.forEach(tag =>
-            cp.purge(`/tags/${tag.id}`).end((err, res) =>
-                st.ok(res.body.meta.ok, `deleted test tag (${tag.id})`)));
+            setTimeout(() => {
+                fixture.tags.forEach((tag, i) => {
+                    setTimeout(() => {
+                        cp.purge(`/tags/${tag.id}`).end((err, res) =>
+                            st.ok(res.body.meta.ok, `deleted test tag (${tag.id})`));
+                    }, 100 * i);
+                });
+            }, 1000);
+        });
     });
 });
