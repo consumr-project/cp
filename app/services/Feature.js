ca5d4f470223fd69cc35d391e634624d857b4ce6
Marcos Minond <minond.marcos@gmail.com>
2016-04-02 14:12:05 -0600

    storing feature overrides in localstorage and managing them from query params

--- app/services/Feature.js
+++ app/services/Feature.js
@@ -2,10 +2,12 @@ angular.module('tcp').service('Feature', [
     'DEBUGGING',
     'CONFIG',
     'lodash',
-    function (DEBUGGING, CONFIG, lodash) {
+    '$location',
+    function (DEBUGGING, CONFIG, lodash, $location) {
         'use strict';
 
-        var KEY_ENABLED = '.enabled';
+        var KEY_ENABLED = '.enabled',
+            NS_FEATURE = 'CPFEATURE(%s, %s, %s)';
 
         var FEATURES = lodash.clone(CONFIG.features);
 
@@ -14,7 +16,8 @@ angular.module('tcp').service('Feature', [
          * @return {Boolean}
          */
         function on(feature) {
-            return Boolean(lodash.get(FEATURES, feature + KEY_ENABLED));
+            return Boolean(localStorage.getItem(ns(feature)) === 'true' ||
+                lodash.get(FEATURES, feature + KEY_ENABLED));
         }
 
         /**
@@ -25,9 +28,48 @@ angular.module('tcp').service('Feature', [
             return !on(feature);
         }
 
+        /**
+         * @param {String} feature
+         * @return {String}
+         */
+        function ns(feature) {
+            return NS_FEATURE
+                .replace('%s', feature)
+                .replace('%s', CONFIG.environment.name)
+                .replace('%s', location.host);
+        }
+
+        /**
+         * @param {String} feature
+         * @return {void}
+         */
+        function enable(feature) {
+            localStorage.setItem(ns(feature), true);
+        }
+
+        /**
+         * @param {String} feature
+         * @return {void}
+         */
+        function disable(feature) {
+            localStorage.setItem(ns(feature), false);
+        }
+
+        lodash.chain([].concat($location.search()._fon))
+            .filter()
+            .map(enable)
+            .value();
+
+        lodash.chain([].concat($location.search()._foff))
+            .filter()
+            .map(disable)
+            .value();
+
         return {
             on: on,
             off: off,
+            enable: enable,
+            disable: disable,
         };
     }
 ]);
