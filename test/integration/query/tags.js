6553c1cec8eac7acf5a44ae17051fcbaa9f67119
Marcos Minond <minond.marcos@gmail.com>
2016-04-20 18:58:12 -0600

    tag tests

---
+++ test/integration/query/tags.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const tapes = require('tapes');
+const http = require('../utils/http');
+const auth = require('../utils/auth');
+
+const find = require('lodash').find;
+const clone = require('lodash').clone;
+const config = require('acm');
+const fixture = clone(config('fixtures'));
+
+tapes('tags', t => {
+    t.plan(1);
+
+    auth.login(fixture.user.admin.auth_apikey).end((err, res) => {
+        t.error(err);
+
+        fixture.tags.forEach(tag => {
+            tag.created_by = res.body.id;
+            tag.updated_by = res.body.id;
+        });
+    });
+
+    reset();
+
+    t.test('setup', st => {
+        st.plan(fixture.tags.length * 2);
+
+        fixture.tags.forEach(tag =>
+            http.post('/service/query/tags', tag).end((err, res) => {
+                st.error(err, 'no error');
+                st.ok(res.body.meta.ok, `created test tag (${tag.id})`);
+            }));
+    });
+
+    t.test('like search', st => {
+        st.plan(5);
+
+        http.get('/service/query/tags/like?s[]=log').end((err, res) => {
+            st.error(err);
+            st.ok(find(res.body.body, { label: 'Logging' }));
+        });
+
+        http.get('/service/query/tags/like?s[]=log&s[]=fish').end((err, res) => {
+            st.error(err);
+            st.ok(find(res.body.body, { label: 'Logging' }));
+            st.ok(find(res.body.body, { label: 'Fishing' }));
+        });
+    });
+
+    reset();
+
+    function reset() {
+        t.test('reset', st => {
+            st.plan(fixture.tags.length);
+
+            fixture.tags.forEach(tag =>
+                http.purge(`/service/query/tags/${tag.id}`).end((err, res) =>
+                    st.ok(res.body.meta.ok, `deleted test tag (${tag.id})`)));
+        });
+    }
+});
