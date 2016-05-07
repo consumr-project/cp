4b4601e3ae1fde78ee350c34d7463a1051396c1d
Marcos Minond <minond.marcos@gmail.com>
2016-05-07 14:25:11 -0600

    enum helper

--- src/auth/permissions.ts
+++ src/auth/permissions.ts
@@ -1,23 +1,19 @@
 import { Configuration as RbacConfiguration } from 'rbac-interface';
 import { clone } from 'lodash';
 
+import { make_enum } from '../utilities';
 import { UnauthorizedError } from '../errors';
 
 import Rbac = require('rbac');
 import config = require('acm');
 
 const RULES = clone(config<RbacConfiguration>('rbac'));
-const ROLES = RULES.roles.reduce(make_enum, {});
+const ROLES = make_enum(RULES.roles);
 
 
 export const rbac = new Rbac(RULES);
 export const roles = ROLES;
 
-
-function make_enum(store: Object, val: string): Object {
-    store[val.toUpperCase()] = val;
-    return store;
-}
 
 export function check(role: string, action: string, resource: string) {
     return new Promise<Boolean>((resolve, reject) => {
---
+++ src/utilities.ts
@@ -0,0 +1,8 @@
+export function make_enum_entry(store: Object, val: string): Object {
+    store[val.toUpperCase()] = val;
+    return store;
+}
+
+export function make_enum(items: string[]): any {
+    return items.reduce(make_enum_entry, {});
+}
