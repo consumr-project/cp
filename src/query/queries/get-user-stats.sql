c650779bf22429fc0fb779d30e734d065f4b34ec
Marcos Minond <minond.marcos@gmail.com>
2016-05-24 22:56:15 -0600

    working on user stats query

--- src/query/queries/get-user-stats.sql
+++ src/query/queries/get-user-stats.sql
@@ -1,12 +1,18 @@
 with contributions as (
     select
         count(distinct c.id)::"numeric" as contributions_companies,
-        count(distinct e.id)::"numeric" as contributions_events
+        count(distinct e.id)::"numeric" as contributions_events,
+        count(distinct r.id)::"numeric" as contributions_reviews,
+        count(distinct q.id)::"numeric" as contributions_questions,
+        count(distinct r.id)::"numeric" as contributions_tags
 
     from users u
 
     left join companies c on (u.id = c.created_by and c.deleted_date is null)
     left join events e on (u.id = e.created_by and e.deleted_date is null)
+    left join reviews r on (u.id = r.created_by and r.deleted_date is null)
+    left join questions q on (u.id = q.created_by and q.deleted_date is null)
+    left join tags t on (u.id = t.created_by and t.deleted_date is null)
 
     where u.id = :id
 
@@ -59,9 +65,15 @@ favorites as (
 )
 
 select
-    cn.contributions_companies + cn.contributions_events as contributions,
+    cn.contributions_companies + cn.contributions_events +
+        cn.contributions_reviews + cn.contributions_questions +
+        cn.contributions_tags as contributions,
+
     cn.contributions_companies,
     cn.contributions_events,
+    cn.contributions_reviews,
+    cn.contributions_questions,
+    cn.contributions_tags,
 
     fg.following_companies + fg.following_tags + fg.following_users as following,
     fg.following_companies,
