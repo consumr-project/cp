07bad6e98926523dddb6c001f605f0766ca345cd
Marcos Minond <minond.marcos@gmail.com>
2016-05-24 22:43:07 -0600

    working on user stats query

--- src/query/queries/get-user-stats.sql
+++ src/query/queries/get-user-stats.sql
@@ -30,6 +30,20 @@ following as (
     where u.id = '4a9cb039-2a8c-458e-839f-78b4d951c226'
 
     group by u.id
+),
+
+followers as (
+    select
+        count(distinct uf.f_user_id)::"numeric" as followers_users
+
+    from users u
+
+    left join user_followers uf on (u.id = uf.f_user_id and uf.deleted_date is null)
+
+    -- where u.id = :user_id
+    where u.id = '4a9cb039-2a8c-458e-839f-78b4d951c226'
+
+    group by u.id
 )
 
 select
@@ -37,13 +51,17 @@ select
     c.contributions_companies,
     c.contributions_events,
 
-    f.following_companies + f.following_tags + f.following_users as following,
-    f.following_companies,
-    f.following_users,
-    f.following_tags
+    fg.following_companies + fg.following_tags + fg.following_users as following,
+    fg.following_companies,
+    fg.following_users,
+    fg.following_tags,
+
+    fr.followers_users as followers,
+    fr.followers_users
 
 from
     contributions c,
-    following f
+    following fg,
+    followers fr
 
 ;
