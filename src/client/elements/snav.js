0f668b01c1d63109f9211ad51a0389531c289163
Marcos Minond <minond.marcos@gmail.com>
2016-06-04 13:05:42 -0600

    auto selectiong values on snav element

--- src/client/elements/snav.js
+++ src/client/elements/snav.js
@@ -4,19 +4,37 @@ angular.module('tcp').directive('snav', function () {
     var template = '<div class="snav"><ng-transclude></ng-transclude></div>';
 
     /**
+     * @param {jQuery} elem
+     * @param {jQuery} child
+     */
+    function select(elem, child) {
+        elem
+            .find('.snav__item')
+            .removeClass('snav__item--active');
+
+        child
+            .closest('.snav__item')
+            .addClass('snav__item--active');
+    }
+
+    /**
+     * @param {number | string} value
+     * @return {string}
+     */
+    function sel_value(value) {
+        return '[value=' + value + ']';
+    }
+
+    /**
      * @param {angular.Scope} scope
      * @param {jQuery} elem
      * @param {angular.Attributes} attrs
      */
     function link(scope, elem, attrs) {
-        elem.on('click', function (ev) {
-            elem
-                .find('.snav__item')
-                .removeClass('snav__item--active');
-
-            angular.element(ev.target)
-                .closest('.snav__item')
-                .addClass('snav__item--active');
+        scope.$watch('value', function (value) {
+            setTimeout(function () {
+                select(elem, elem.find(sel_value(value)));
+            }, 10);
         });
     }
 
@@ -25,5 +43,8 @@ angular.module('tcp').directive('snav', function () {
         transclude: true,
         template: template,
         link: link,
+        scope: {
+            value: '=',
+        },
     };
 });
