572bb04e2f3cc93f23ff90713ae07ee71618f616
Marcos Minond <minond.marcos@gmail.com>
2016-06-09 17:27:19 -0500

    fixing messages test

--- test/src/message/stringify.js
+++ test/src/message/stringify.js
@@ -6,8 +6,8 @@ const I18n = require('../utils/i18n');
 const Message = require('../../../build/src/notification/message').default;
 const stringify = require('../../../build/src/client/services/messages').stringify;
 
-const MTYPE = require('../../../build/src/notification/message').TYPE;
-const NTYPE = require('../../../build/src/notification/notification').TYPE;
+const CATEGORY = require('../../../build/src/notification/message').CATEGORY;
+const NOTIFICATION = require('../../../build/src/notification/message').NOTIFICATION;
 
 const i18n = new I18n({
     'notification/followed_by_one': '<b>{name}</b> started following you.',
@@ -16,9 +16,11 @@ const i18n = new I18n({
 test('message stringify', t => {
     t.plan(1);
     t.equal('<b>Marcos Minond</b> started following you.', stringify(i18n, [
-        new Message({ type: MTYPE.NOTIFICATION, payload: {
-            type: NTYPE.FOLLOWED,
-            name: 'Marcos Minond',
-        }})
+        new Message(CATEGORY.NOTIFICATION, NOTIFICATION.FOLLOWED,
+            '1ec14c84-eb3d-41b3-b86a-3b28f2cf9853', {
+                id: '1ec14c84-eb3d-41b3-b86a-3b28f2cf9853',
+                otype: 'user',
+                name: 'Marcos Minond',
+            })
     ]));
 });
