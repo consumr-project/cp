b1bb17b6214d5a41ab683277f9f4e09e621f84a0
Marcos Minond <minond.marcos@gmail.com>
2016-06-09 00:27:47 -0500

    notification builder

--- src/client/services/messages.ts
+++ src/client/services/messages.ts
@@ -1,23 +1,26 @@
-import { head } from 'lodash';
+import { get } from 'lodash';
+import { I18n } from 'cp/client';
 
 import { TYPE } from '../../notification/notification';
 import Message from '../../notification/message';
 
-function stringify_followed(messages: Message[]): string {
-    switch (messages.length) {
-    case 1:
-        return `${messages[0].payload.name} started following you`;
-    }
+function stringify_followed(i18n: I18n, messages: Message[]): string {
+    var fields = {
+        name: get(messages, '0.payload.name'),
+        other: get(messages, '1.payload.name'),
+        count: messages.length - 1,
+    };
 
-    return '';
+    switch (messages.length) {
+    case 1: return i18n.get('notification/followed_by_one', fields);
+    case 2: return i18n.get('notification/followed_by_two', fields);
+    default: return i18n.get('notification/followed_by_many', fields);
+    }
 }
 
-export function stringify(messages: Message[]): string {
-    var sample = head(messages);
-
-    switch (sample.payload.type) {
-    case TYPE.FOLLOWED: return stringify_followed(messages);
+export function stringify(i18n: I18n, messages: Message[]): string {
+    switch (messages[0].payload.type) {
+    case TYPE.FOLLOWED: return stringify_followed(i18n, messages);
+    default: return '';
     }
-
-    return '';
 }
