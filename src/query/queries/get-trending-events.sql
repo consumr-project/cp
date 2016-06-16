45a7d980234e1b384b1b20c37f2ac59bb69b45c8
Marcos Minond <marcos.minond@domo.com>
2016-06-15 20:30:54 -0600

    adding tag info to trending events query

--- src/query/queries/get-trending-events.sql
+++ src/query/queries/get-trending-events.sql
@@ -1,11 +1,16 @@
 select eb.event_id as id,
     count(eb.event_id) as score,
     max(eb.updated_date) as updated_date,
-    e.title
+    e.title,
+
+    array_agg(t.id) as tag_ids,
+    array_agg(t."en-US") as tag_labels
 
 from event_bookmarks eb
 
 left join events e on (eb.event_id = e.id)
+left join event_tags et on (et.event_id = e.id)
+left join tags t on (et.tag_id = t.id)
 
 where eb.deleted_date is null
 
