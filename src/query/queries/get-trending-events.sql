dee27cd138d625b7ddd77ab3e565225ff1b085a3
Marcos Minond <marcos.minond@domo.com>
2016-06-15 20:35:48 -0600

    adding company info to trending events query

--- src/query/queries/get-trending-events.sql
+++ src/query/queries/get-trending-events.sql
@@ -3,14 +3,19 @@ select eb.event_id as id,
     max(eb.updated_date) as updated_date,
     e.title,
 
-    array_agg(t.id) as tag_ids,
-    array_agg(t."en-US") as tag_labels
+    -- XXX id and labels are being re-ordered by `distinct` and don't match up
+    array_agg(distinct t.id) as tag_ids,
+    array_agg(distinct t."en-US") as tag_labels,
+    array_agg(distinct c.id) as company_ids,
+    array_agg(distinct c.name) as company_labels
 
 from event_bookmarks eb
 
 left join events e on (eb.event_id = e.id)
 left join event_tags et on (et.event_id = e.id)
 left join tags t on (et.tag_id = t.id)
+left join company_events ce on (ce.event_id = e.id)
+left join companies c on (ce.company_id = c.id)
 
 where eb.deleted_date is null
 
