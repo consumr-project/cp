ea6182810d5b788aebe533cd435d8df4f7a27bf3
Marcos Minond <marcos.minond@domo.com>
2016-06-15 21:44:00 -0600

    event spacing

--- src/client/components/source.js
+++ src/client/components/source.js
@@ -5,7 +5,7 @@ angular.module('tcp').directive('source', [
         var HTML_VIEW = [
             '<div class="source source--view line-separated">',
             '    <a target="_blank" rel="noreferrer" href="{{::model.url}}">{{::model.url}}</a>',
-            '    <h4 i18n date="{{::model.published_date}}" format="D MMM, YYYY" class="margin-top-xsmall"></h4>',
+            '    <h4 i18n date="{{::model.published_date}}" format="D MMM, YYYY" class="margin-top-small"></h4>',
             '    <p>{{::model.summary}}</p>',
             '</div>',
         ].join('');
