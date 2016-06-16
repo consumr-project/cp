a6c14cb9f8f3069916b7c1c004a3ea68ca428f0f
Marcos Minond <marcos.minond@domo.com>
2016-06-15 20:26:09 -0600

    adding event title to trending events query

--- src/query/queries/get-trending-events.sql
+++ src/query/queries/get-trending-events.sql
@@ -1,13 +1,18 @@
 select eb.event_id as id,
     count(eb.event_id) as score,
-    max(eb.updated_date) as updated_date
+    max(eb.updated_date) as updated_date,
+    e.title
 
 from event_bookmarks eb
 
+left join events e on (eb.event_id = e.id)
+
 where eb.deleted_date is null
 
-group by eb.event_id
+group by eb.event_id,
+    e.title
 
-order by score desc, updated_date desc
+order by score desc,
+    updated_date desc
 
 ;
