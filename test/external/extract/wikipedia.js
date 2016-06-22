ab706e364281c5c8a0c10f0c56ffeb4fabf2a360
Marcos Minond <marcos.minond@domo.com>
2016-06-21 19:27:24 -0600

    fixing external integration test

--- test/external/extract/wikipedia.js
+++ test/external/extract/wikipedia.js
@@ -19,11 +19,15 @@ tape('wiki', t => {
         t.equal(33589, res.body.body.id);
     });
 
-    http.get('/service/extract/wiki/infobox?q=Walmart').end((err, res) => {
-        t.comment('infobox');
-        check.rescheck(t, err, res);
-        t.equal('Walmart Stores, Inc.', res.body.body.infobox.name);
-    });
+    // XXX some changes being currently done to Walmart's re the name that
+    // should be used in their infobox. commenting out until that's settled.
+    // https://en.wikipedia.org/wiki/Walmart
+    // https://en.wikipedia.org/w/index.php?title=Walmart&action=history
+    // http.get('/service/extract/wiki/infobox?q=Walmart').end((err, res) => {
+    //     t.comment('infobox');
+    //     check.rescheck(t, err, res);
+    //     t.equal('Walmart Stores, Inc.', res.body.body.infobox.name);
+    // });
 
     http.get('/service/extract/wiki/infobox?q=Walmart&parts=urls').end((err, res) => {
         t.comment('infobox with parts');
