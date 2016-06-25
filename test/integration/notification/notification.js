8b85d0a6ac314975fd9ea7c8ea44896ed7a444b2
Marcos Minond <minond.marcos@gmail.com>
2016-06-25 12:15:22 -0600

    more notification tests

--- test/integration/notification/notification.js
+++ test/integration/notification/notification.js
@@ -6,25 +6,64 @@ const auth = require('../utils/auth');
 
 const map = require('lodash').map;
 const find = require('lodash').find;
+const head = require('lodash').head;
 const clone = require('lodash').clone;
+
 const config = require('acm');
 const fixture = clone(config('fixtures'));
 
 tapes('notifications', t => {
     var ids_to_look_for = [],
-        ids_to_delete = [];
+        ids_to_delete = [],
+        event = head(fixture.events);
 
     t.plan(1);
 
     auth.login(fixture.user.admin.auth_apikey).end((err, res) => {
         t.error(err);
+        event.created_by = res.body.id;
+        event.updated_by = res.body.id;
+    });
+
+    t.test('setup', st => {
+        st.plan(3);
+
+        http.purge(`/service/record/events/${event.id}`).end((err, res) => {
+            st.ok(res.body.meta.ok, 'can delete an event');
+
+            http.post('/service/record/events', event).end((err, res) => {
+                st.error(err);
+                st.ok(res.body.meta.ok, 'can create an event');
+            });
+        });
     });
 
     t.test('create', st => {
-        st.plan(1);
+        st.plan(4);
 
         http.post('/service/notification/follow', {
             id: fixture.user.admin.id,
+        }).end((err, res) => {
+            st.error(err);
+            ids_to_look_for.push(res.body.body.id);
+        });
+
+        http.post('/service/notification/favorite', {
+            id: event.id,
+        }).end((err, res) => {
+            st.error(err);
+            ids_to_look_for.push(res.body.body.id);
+        });
+
+        http.post('/service/notification/contribute', {
+            id: event.id,
+        }).end((err, res) => {
+            st.error(err);
+            ids_to_look_for.push(res.body.body.id);
+        });
+
+        http.post('/service/notification/modify', {
+            id: event.id,
         }).end((err, res) => {
             st.error(err);
             ids_to_look_for.push(res.body.body.id);
@@ -44,12 +83,15 @@ tapes('notifications', t => {
     });
 
     t.test('purge', st => {
-        st.plan(ids_to_delete.length);
+        st.plan(ids_to_delete.length + 1);
 
         ids_to_delete.forEach(id => {
             http.purge(`/service/notification/${id}`).end((err, res) => {
                 st.error(err);
             });
         });
+
+        http.del(`/service/record/events/${fixture.events[0].id}`).end((err, res) =>
+            st.ok(res.body.meta.ok, 'can delete an event'));
     });
 });
