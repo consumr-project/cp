5af69211651ed65a8dd09d94b59540794df4c901
Marcos Minond <minond.marcos@gmail.com>
2016-08-19 16:55:05 -0600

    moved cp module into service/http

--- src/service/http.ts
+++ src/service/http.ts
@@ -3,14 +3,14 @@ import { Request, Response } from 'express';
 export type ServiceRequestHandler = (req: Request, res: Response, next: (err?: Error) => {}) => void;
 export type ServiceRequestPromise<T> = (req: Request, res: Response, next: (err?: Error) => {}) => Promise<T>;
 
-export interface ServiceResponseV1<T> {
-    body: T | Array<T> | { [index: string]: T };
-    meta: ServiceResultMetadata;
-}
-
 export interface ServiceResultMetadata {
     ok: boolean;
     took?: number;
     from_cache?: boolean;
     message?: string;
+}
+
+export interface ServiceResponseV1<T> {
+    body: T | T[] | { [index: string]: T };
+    meta: ServiceResultMetadata;
 }
