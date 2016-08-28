572e0195086934cd9902e6495762d53967881f30
Marcos Minond <marcos.minond@domo.com>
2016-08-27 21:03:42 -0600

    fixing unit test

--- test/integration/utils/crypto.js
+++ test/integration/utils/crypto.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const crypto = require('../../../build/crypto');
+const KEY_USER_EMAIL = require('../../../build/keys').KEY_USER_EMAIL;
 
 /**
  * @param {String} em
  * @return {String}
  */
 function email(em) {
-    return crypto.encrypt(em, crypto.KEY_USER_EMAIL);
+    return crypto.encrypt(em, KEY_USER_EMAIL);
 }
 
 module.exports = { email };
