e935ab57c418b49699887c57b5d72a4283d0b1bc
Marcos Minond <marcos.minond@domo.com>
2016-08-29 00:18:34 -0600

    making service/dbms docker friendlier

--- src/service/dbms.ts
+++ src/service/dbms.ts
@@ -5,8 +5,14 @@ import debug = require('debug');
 
 export type DatabaseConnection = Sequelize;
 
+const docker_url = 'postgres://' + process.env.POSTGRES_USER +
+    ':' + process.env.POSTGRES_PASSWORD +
+    '@' + process.env.POSTGRES_PORT_5432_TCP_ADDR +
+    ':' + process.env.POSTGRES_PORT_5432_TCP_PORT +
+    '/' + process.env.POSTGRES_DB;
+
 export default (c = config): DatabaseConnection =>
-    new Connection(c('database.url'), {
+    new Connection(c('database.url') || docker_url, {
         logging: debug('cp:service:dbms'),
         pool: c('database.pool')
     });
