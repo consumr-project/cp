0f488ca3761e3326a20154bca9dfc21eb3809ee7
Marcos Minond <marcos.minond@domo.com>
2016-08-30 00:38:08 -0600

    migrations in docker

--- config/migrations.sh
+++ config/migrations.sh
@@ -1,9 +1,16 @@
 source script/cli.sh
 
-hostname=$(url:parse $DATABASE_URL hostname)
-username=$(url:parse $DATABASE_URL auth | cut -d':' -f 1)
-database=$(node -e "console.log(require('acm')('database.url').split('/').pop())")
 userflag=""
+
+if [ ! -z "$DATABASE_URL" ]; then
+    hostname=$(url:parse $DATABASE_URL hostname)
+    username=$(url:parse $DATABASE_URL auth | cut -d':' -f 1)
+    database=$(node -e "console.log(require('acm')('database.url').split('/').pop())")
+else
+    hostname="$POSTGRES_HOST"
+    username="$POSTGRES_USER"
+    database="$POSTGRES_DB"
+fi
 
 if [ ! -z "$username" ]; then
     userflag="-U $username"
