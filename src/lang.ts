df38788207960533547c8a0ddcad79027ffc9161
Marcos Minond <minond.marcos@gmail.com>
2016-09-02 16:54:15 -0600

    token create endpoint

--- src/lang.ts
+++ src/lang.ts
@@ -8,3 +8,4 @@ export const Millisecond: Duration = 1;
 export const Second = Millisecond * 1000;
 export const Minute = Second * 60;
 export const Hour = Minute * 60;
+export const Day = Hour * 24;
--- src/server/auth.ts
+++ src/server/auth.ts
@@ -3,16 +3,20 @@ import { User as UserMessage } from 'cp/record';
 
 import * as express from 'express';
 import * as passport from 'passport';
-import { User } from '../service/models';
-import { roles } from '../auth/permissions';
+import { User, Token } from '../service/models';
+import { can, roles } from '../auth/permissions';
 import linkedin_auth from '../auth/linkedin';
 import apikey_auth from '../auth/apikey';
+import { Day } from '../lang';
+import { Manager } from '../auth/token';
+import { service_handler } from '../service/http';
 
 export var app = express();
 export { passport };
 
-var linkedin = linkedin_auth();
-var apikey = apikey_auth();
+const token = new Manager(Token);
+const linkedin = linkedin_auth();
+const apikey = apikey_auth();
 
 passport.serializeUser(serialize);
 passport.deserializeUser(deserialize);
@@ -21,8 +25,18 @@ passport.use(apikey.strategy);
 
 app.get('/user', (req, res) => res.json(req.user || {}));
 app.get('/logout', (req, res, next) => { req.logout(); next(); }, js_update_client_auth);
-app.get('/linkedin', linkedin.pre_base, linkedin.login);
+
+app.get('/linkedin', (req, res, next) => {
+}, linkedin.pre_base, linkedin.login);
 app.get('/linkedin/callback', linkedin.callback, js_update_client_auth);
+
+app.post('/token',
+    can('create', 'token'),
+    service_handler(req => token.generate(
+        new Date(req.body.expiration_date || Date.now() + Day * 30),
+        req.body.reason,
+        req.user
+    )));
 
 if (process.env.CP_ALLOW_APIKEY_AUTH) {
     app.post('/key', apikey.login, (req, res) => res.json(req.user || {}));
