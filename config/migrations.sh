2b9a38734992ea9e8a22002b11c0053bf580414d
Marcos Minond <minond.marcos@gmail.com>
2016-09-01 21:14:05 -0600

    using db url in migration script

--- config/migrations.sh
+++ config/migrations.sh
@@ -1,20 +1,25 @@
 source script/cli.sh
 
 userflag=""
+db_cmd=""
 
 if [ ! -z "$DATABASE_URL" ]; then
     hostname=$(url:parse $DATABASE_URL hostname)
     username=$(url:parse $DATABASE_URL auth | cut -d':' -f 1)
     database=$(node -e "console.log(require('acm')('database.url').split('/').pop())")
+
+    db_cmd="psql $DATABASE_URL"
 else
     hostname="$POSTGRES_HOST"
     username="$POSTGRES_USER"
     database="$POSTGRES_DB"
+
+    if [ ! -z "$username" ]; then
+        userflag="-U $username"
+    fi
+
+    db_cmd="psql -h $hostname $userflag $database -f"
 fi
 
-if [ ! -z "$username" ]; then
-    userflag="-U $username"
-fi
-
-[ -z "$DB_CMD" ] && DB_CMD="psql -h $hostname $userflag $database -f"
+[ -z "$DB_CMD" ] && DB_CMD="$db_cmd"
 [ -z "$JS_CMD" ] && JS_CMD="node"
