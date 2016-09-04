c394171a6a81836e18783753a8a75464691b5029
Marcos Minond <minond.marcos@gmail.com>
2016-09-04 15:26:55 -0600

    fixing broken migration

--- config/migrations/1472945467-allowed-emails.sql
+++ config/migrations/1472945467-allowed-emails.sql
@@ -1,27 +1,36 @@
-create table if not exists allowed_emails (
-    email varchar(100) not null,
+do $$
+begin
+    if not exists (
+        select 1
+        from information_schema.tables
+        where table_name = 'beta_email_invites'
+    ) then
+        create table if not exists allowed_emails (
+            email varchar(100) not null,
 
-    created_by uuid not null,
-    created_date timestamp with time zone not null,
+            created_by uuid not null,
+            created_date timestamp with time zone not null,
 
-    updated_by uuid not null,
-    updated_date timestamp with time zone not null default now(),
+            updated_by uuid not null,
+            updated_date timestamp with time zone not null default now(),
 
-    deleted_by uuid,
-    deleted_date timestamp with time zone,
+            deleted_by uuid,
+            deleted_date timestamp with time zone,
 
-    constraint allowed_emails_pkey
-        primary key (email),
+            constraint allowed_emails_pkey
+                primary key (email),
 
-    constraint allowed_emails_created_by_fkey
-        foreign key (created_by) references users(id)
-            on update cascade,
+            constraint allowed_emails_created_by_fkey
+                foreign key (created_by) references users(id)
+                    on update cascade,
 
-    constraint allowed_emails_updated_by_fkey
-        foreign key (updated_by) references users(id)
-            on update cascade,
+            constraint allowed_emails_updated_by_fkey
+                foreign key (updated_by) references users(id)
+                    on update cascade,
 
-    constraint allowed_emails_deleted_by_fkey
-        foreign key (deleted_by) references users(id)
-            on update cascade
-);
+            constraint allowed_emails_deleted_by_fkey
+                foreign key (deleted_by) references users(id)
+                    on update cascade
+        );
+    end if;
+end$$;
