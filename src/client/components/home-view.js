24885443a66f5a272dc6c4cdcf9be93c06930ad7
Marcos Minond <minond.marcos@gmail.com>
2016-09-14 21:32:37 -0600

    your stuff in home page

--- src/client/components/home-view.js
+++ src/client/components/home-view.js
@@ -1,8 +1,16 @@
 angular.module('tcp').directive('homeView', [
-    function () {
+    'Session',
+    function (Session) {
         'use strict';
 
         function controller($scope, $window) {
+            $scope.vm = {
+                selected: Session.USER && Session.USER.id ? 'mine' : 'trending',
+
+                TYPE_MINE: 'mine',
+                TYPE_TRENDING: 'trending',
+            };
+
             $scope.im_cool = function () {
                 $window.alert('ok, thanks for being cool but I\'m going to ' +
                     'need you to be cool in like three more weeks when ' +
@@ -23,11 +31,15 @@ angular.module('tcp').directive('homeView', [
                     '        </div>',
                     '    </div>',
 
-                    '    <section class="margin-top-large home-view-component__wrapper">',
-                    '        <div class="snav__item snav__item--active margin-left-xlarge" i18n="home/trending"></div>',
+                    '    <section class="margin-top-xsmall home-view-component__wrapper">',
+                    '        <snav class="snav--borderless" value="vm.selected">',
+                    '            <snav-item ng-click="vm.selected = vm.TYPE_TRENDING" value="{{vm.TYPE_TRENDING}}" class="margin-left-xlarge" i18n="home/trending"></snav-item>',
+                    '            <snav-item ng-click="vm.selected = vm.TYPE_MINE" value="{{vm.TYPE_MINE}}" class="logged-in-only--display" i18n="home/your_stuff"></snav-item>',
+                    '        </snav>',
                     '        <hr>',
 
-                    '        <trending type="trending"></trending>',
+                    '        <trending ng-if="vm.selected === vm.TYPE_TRENDING" type="trending"></trending>',
+                    '        <trending ng-if="vm.selected === vm.TYPE_MINE" type="mine"></trending>',
 
                     '        <div class="home-view-component__coooool">',
                     '            <p i18n="home/have_a_sec"></p>',
