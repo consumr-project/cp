e8c43858d67d3d1b4d6482033c79e15e2e6f57a0
Marcos Minond <minond.marcos@gmail.com>
2016-09-21 20:41:14 -0600

    moving to new logger

--- src/worker/river.ts
+++ src/worker/river.ts
@@ -3,9 +3,10 @@ import es_connect from '../device/elasticsearch';
 import { get, elasticsearch } from '../search/updater';
 import { LinkConfiguration, LinkDefinition } from '../river/sync';
 import { Duration } from '../lang';
+import { logger } from '../log';
 import * as config from 'acm';
 
-const log = require('debug')('cp:worker:river');
+const log = logger(__filename);
 const models: LinkDefinition[] = config('river.models').map((def: LinkConfiguration) =>
     new LinkDefinition(def.name, def.fields, def.soft_delete,
         def.primary_key, def.label));
@@ -14,7 +15,7 @@ export default function (since: Duration) {
     var db = db_connect(),
         es = es_connect();
 
-    models.map(model => log(model));
+    models.map(model => log.info('river model entry', model));
     Promise.all(models.map(model => get(db, model, { since })
         .then(rows => elasticsearch(es, model, rows))
         .then(ack => log('done updating %s', model.name))))
