283cef01c52916b18cf39336d8a52d803ef91182
Marcos Minond <minond.marcos@gmail.com>
2016-09-25 14:02:21 -0600

    connecting to elasticsearch on start up

--- src/server/search.ts
+++ src/server/search.ts
@@ -1,5 +1,5 @@
 import * as express from 'express';
-import es from '../device/elasticsearch';
+import es_connect from '../device/elasticsearch';
 
 import { conn } from '../device/models';
 import { sql, query } from '../record/query';
@@ -7,9 +7,10 @@ import { fuzzy, normalize, INDEX, TYPE } from '../search/searcher';
 import { service_handler } from '../http';
 
 export var app = express();
+const es = es_connect();
 
 app.get('/query', query(conn, sql('search')));
-app.get('/fuzzy', service_handler(req => fuzzy(es(), {
+app.get('/fuzzy', service_handler(req => fuzzy(es, {
     index: INDEX.RECORD,
     query: req.query.q,
     type: [TYPE.COMPANIES, TYPE.USERS, TYPE.TAGS],
