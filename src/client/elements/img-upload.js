e5a114b395389b2927464a68f29d205bd7831be5
Marcos Minond <minond.marcos@gmail.com>
2016-10-31 17:49:04 -0600

    working on upload

--- src/client/elements/img-upload.js
+++ src/client/elements/img-upload.js
@@ -7,8 +7,20 @@ angular.module('tcp').directive('imgUpload', [
     function (Dropzone, Webcam, i18n) {
         'use strict';
 
+        var UPLOAD_CONFIG = {
+            url: '/service/extract/page'
+        };
+
+        var WEBCAM_CONFIG = {
+            width: 320,
+            height: 240,
+            dest_width: 640,
+            dest_height: 480,
+        };
+
         var template = [
             '<div class="img-upload">',
+            '    <div class="img-upload__upload"></div>',
             '    <div class="img-upload__webcam"></div>',
             '</div>',
         ].join('');
@@ -20,16 +32,18 @@ angular.module('tcp').directive('imgUpload', [
          * @return {void}
          */
         function link(scope, elem, attrs) {
-            Webcam.reset();
+            var upload_node = elem.find('.img-upload__upload').get(0);
+            var webcam_node = elem.find('.img-upload__webcam').get(0);
 
-            Webcam.set({
-                width: 320,
-                height: 240,
-                dest_width: 640,
-                dest_height: 480,
-            });
+            var upload = new Dropzone(upload_node, UPLOAD_CONFIG);
+            var webcam = Object.create(Webcam);
 
-            Webcam.attach(elem.find('.img-upload__webcam').get(0));
+            webcam.reset();
+            webcam.set(WEBCAM_CONFIG);
+            webcam.attach(webcam_node);
+
+            scope.upload = upload;
+            scope.webcam = webcam;
         }
 
         /**
