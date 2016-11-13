328f21f52cf01bc0f9c76b78ea9e13eecba05db5
Marcos Minond <marcos.minond@domo.com>
2016-11-12 21:26:29 -0700

    image upload flow

--- src/client/elements/img-upload.js
+++ src/client/elements/img-upload.js
@@ -7,7 +7,8 @@ angular.module('tcp').directive('imgUpload', [
     'utils',
     'utils2',
     'assert',
-    function (Dropzone, Webcam, i18n, utils, utils2, assert) {
+    'messages',
+    function (Dropzone, Webcam, i18n, utils, utils2, assert, messages) {
         'use strict';
 
         var UPLOAD_CONFIG = {
@@ -54,7 +55,7 @@ angular.module('tcp').directive('imgUpload', [
             '                </div>',
             '            </td>',
             '        </tr>',
-            '        <tr ng-show="vm.can_submit">',
+            '        <tr ng-show="vm.show_controls">',
             '            <td colspan="2" class="img-upload__footer">',
             '                <button i18n="admin/submit"',
             '                    ng-click="submit()"',
@@ -62,6 +63,9 @@ angular.module('tcp').directive('imgUpload', [
             '                <button i18n="admin/cancel"',
             '                    class="button--link"',
             '                    ng-click="cancel()"></button>',
+            '                <span class="status--error">{{vm.error_msg}}</span>',
+            '                <span class="status--success">{{vm.success_msg}}</span>',
+            '                <span ng-show="vm.loading_msg" class="status--message loading--spinner padding-right-large">{{vm.loading_msg}}</span>',
             '            </td>',
             '        </tr>',
             '    </table>',
@@ -96,7 +100,11 @@ angular.module('tcp').directive('imgUpload', [
 
             scope.vm = {
                 can_submit: false,
+                show_controls: false,
                 running_cam: false,
+                error_msg: null,
+                loading_msg: null,
+                success_msg: null,
             };
 
             function start_cam() {
@@ -120,6 +128,7 @@ angular.module('tcp').directive('imgUpload', [
                 img_file = null;
 
                 scope.vm.can_submit = true;
+                scope.vm.show_controls = true;
                 scope.$apply();
             }
 
@@ -135,6 +144,7 @@ angular.module('tcp').directive('imgUpload', [
                 img_data = null;
 
                 scope.vm.can_submit = true;
+                scope.vm.show_controls = true;
                 scope.$apply();
             }
 
@@ -147,30 +157,42 @@ angular.module('tcp').directive('imgUpload', [
                 // upload;
                 // debugger;
 
-                if (img_file) {
-                    upload.uploadFile(img_file);
+                scope.vm.can_submit = false;
+                scope.vm.error_msg = null;
+                scope.vm.success_msg = null;
+                scope.vm.loading_msg = i18n.get('user/uploading_photo')
 
+                upload.off('complete');
+                upload.off('success');
+                upload.off('error');
+
+                if (img_file) {
                     upload.on('complete', function () {
                         console.log('img_file complete');
+                        scope.vm.loading_msg = null;
+                        scope.vm.can_submit = true;
+                        scope.$apply();
                     });
 
                     upload.on('success', function () {
-                        console.log('img_file success');
+                        scope.vm.success_msg = i18n.get('user/uploaded_photo')
                     });
 
-                    upload.on('error', function () {
-                        console.log('img_file error');
+                    upload.on('error', function (file, body, res) {
+                        scope.vm.error_msg = messages.from_http_error(i18n, res.status);
+                        scope.$apply();
                     });
+
+                    upload.uploadFile(img_file);
                 // } else if (img_data) {
                 }
-
-                cancel();
             }
 
             function cancel() {
                 Webcam.reset();
                 upload.removeAllFiles(true);
                 scope.vm.can_submit = false;
+                scope.vm.show_controls = false;
                 scope.vm.running_cam = false;
             }
         }
