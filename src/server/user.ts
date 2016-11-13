cdc74194161df39921769e06685f8a210e9a3b78
Marcos Minond <marcos.minond@domo.com>
2016-11-12 17:30:59 -0700

    fixing linting error

--- src/server/user.ts
+++ src/server/user.ts
@@ -1,13 +1,25 @@
 import * as express from 'express';
 import * as multer from 'multer';
 
-import { upload, url } from '../user/avatar';
+import { upload, url, valid_image_type, SUPPORTED_MIMES } from '../user/avatar';
 import { service_handler, service_redirect } from '../http';
-import { BadRequestError, ERR_MSG_MISSING_FIELDS } from '../errors';
+import { UnprocessableEntityError, BadRequestError,
+    ERR_MSG_MISSING_FIELDS, ERR_MSG_INVALID_ENTITY_TYPE } from '../errors';
 
 export const app = express();
 const storage = multer.memoryStorage();
-const uploader = multer({ storage });
+const uploader = multer({ storage, fileFilter: filter });
+
+function filter(req, file, cb) {
+    if (!valid_image_type(file)) {
+        cb(new UnprocessableEntityError(ERR_MSG_INVALID_ENTITY_TYPE(
+            file.mimetype,
+            SUPPORTED_MIMES
+        )), false);
+    } else {
+        cb(null, true);
+    }
+}
 
 app.get('/avatar', service_redirect((req, res, next) => {
     var query: any = {};
