289b6a8568eb1ecca95d393e87ff14fbea672957
Marcos Minond <minond.marcos@gmail.com>
2016-11-27 21:19:25 -0700

    refactoring validation test

--- test/src/validate.js
+++ test/src/validate.js
@@ -4,96 +4,67 @@ const test = require('tape');
 const validate = require('../../build/src/validate').default;
 
 test('validate', t => {
-    t.plan(2);
+    var validation, value1, value2;
 
-    var validation = validate({
+    t.plan(16);
+
+    validation = validate({
         value: () => false,
     });
-
-    t.comment('starts out as undefined');
-    t.equal(undefined, validation.checks.value);
-
+    t.equal(undefined, validation.checks.value, 'starts out as undefined');
     validation.validate();
-    t.comment('is updated on validate call');
-    t.equal(false, validation.checks.value);
-});
 
-test('validate', t => {
-    t.plan(2);
+    t.equal(false, validation.checks.value, 'is updated on validate call');
 
-    var validation = validate({
+    validation = validate({
         fail: () => false,
         pass: () => true,
     }, true);
+    t.equal(true, validation.checks.pass, 'can run validation on creation (1)');
+    t.equal(false, validation.checks.fail, 'can run validation on creation (2)');
 
-    t.comment('can run validation on creation');
-    t.equal(true, validation.checks.pass);
-    t.equal(false, validation.checks.fail);
-});
-
-test('validate', t => {
-    t.plan(4);
-
-    var validation = validate({
+    validation = validate({
         fail: () => false,
         pass: () => true,
     });
-
     validation.validate();
-    t.comment('starts with initial values');
-    t.equal(true, validation.checks.pass);
-    t.equal(false, validation.checks.fail);
+    t.equal(true, validation.checks.pass, 'starts with initial values (1)');
+    t.equal(false, validation.checks.fail, 'starts with initial values (2)');
 
     validation.reset();
-    t.comment('removed after reset');
-    t.equal(undefined, validation.checks.pass);
-    t.equal(undefined, validation.checks.fail);
-});
+    t.equal(undefined, validation.checks.pass, 'removed after reset (1)');
+    t.equal(undefined, validation.checks.fail, 'removed after reset (2)');
 
-test('validate', t => {
-    t.plan(3);
-
-    var value = true;
-
-    var validation = validate({
-        value: () => !!value,
+    value1 = true;
+    validation = validate({
+        value1: () => !!value1,
     }, true);
+    t.equal(true, validation.checks.value1, 'initial check is set');
 
-    t.comment('initial check is set');
-    t.equal(true, validation.checks.value);
+    value1 = false;
+    t.equal(true, validation.checks.value1, 'external update does not change check');
 
-    value = false;
-    t.comment('external update does not change check');
-    t.equal(true, validation.checks.value);
-
-    value = false;
+    value1 = false;
     validation.validate();
-    t.comment('only calling validate function does');
-    t.equal(false, validation.checks.value);
-});
+    t.equal(false, validation.checks.value1, 'only calling validate function does');
 
-test('validate', t => {
-    t.plan(5);
-
-    var value1 = true,
-        value2 = true;
-
-    var validation = validate({
+    validation = validate({
         value1: () => !!value1,
         value2: () => !!value2,
     });
-
-    t.equal(true, validation.validate());
+    value1 = true;
+    value2 = true;
+    t.equal(true, validation.validate(), 'passes when all options are true');
 
     value2 = false;
-    t.equal(false, validation.validate());
+    t.equal(false, validation.validate(), 'fails when an option in false');
 
     value1 =  false;
-    t.equal(false, validation.validate());
+    t.equal(false, validation.validate(), 'fails when all options are false');
 
     value2 = true;
-    t.equal(false, validation.validate());
+    t.equal(false, validation.validate(), 'still failing');
 
     value1 =  true;
-    t.equal(true, validation.validate());
+    t.equal(true, validation.validate(), 'now all options are true and it passes');
 });
