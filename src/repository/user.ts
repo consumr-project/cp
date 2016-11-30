6ad0437e22d95a55423a1d3cff6d2fcdbaa7801b
Marcos Minond <marcos.minond@domo.com>
2016-11-29 19:00:42 -0700

    encrypting user emails before updates

--- src/repository/user.ts
+++ src/repository/user.ts
@@ -1,13 +1,27 @@
-import { merge } from 'lodash';
+import { clone, merge } from 'lodash';
 
 import { Record, User } from '../device/models';
 import { UserMessage } from '../record/models/user';
+import { encrypt } from '../crypto';
+import { KEY_USER_EMAIL } from '../keys';
+
+function prep(update: UserMessage): UserMessage {
+    var { email } = update;
+    update = clone(update);
+
+    if (email) {
+        update.email = encrypt(email, KEY_USER_EMAIL);
+    }
+
+    return update;
+}
 
 export function update_user(user: Record, update: UserMessage): Promise<UserMessage> {
     var { id } = user;
 
+
     return new Promise((resolve, reject) => {
-        User.update(update, { where: { id } })
+        User.update(prep(update), { where: { id } })
             .then(() => resolve(merge(update, { id })))
             .catch(err => reject(err));
     });
