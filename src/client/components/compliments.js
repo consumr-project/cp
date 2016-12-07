1e87953aca0507683e29e887e3c769cae20194e1
Marcos Minond <minond.marcos@gmail.com>
2016-12-06 18:38:38 -0700

    working on compliments

--- src/client/components/compliments.js
+++ src/client/components/compliments.js
@@ -1,10 +1,15 @@
 angular.module('tcp').component('compliments', {
     bindings: {},
     template: [
-        '<popover class="popover--fullscreen" api="$ctrl.popover">',
+        '<popover with-close-x class="popover--fullscreen" api="$ctrl.popover">',
             '<div class="site-content site-content--slim-100 site-content--no-header center-align">',
                 '<h1 class="italic">{{$ctrl.message}}</h1>',
                 '<img ng-src="/assets/images/{{$ctrl.image}}.svg" />',
+                '<button i18n="common/thanks_keep_going" ',
+                    'ng-click="$ctrl.hide()"></button>',
+                '<button i18n="common/enough_stop_now" ',
+                    'ng-click="$ctrl.hide_and_stop()" ',
+                    'class="button--link"></button>',
             '</div>',
         '</popover>',
     ].join(''),
@@ -13,6 +18,8 @@ angular.module('tcp').component('compliments', {
         '$interval',
         function (i18n, $interval) {
             'use strict';
+
+            var KEY = 'cp:no_compliments';
 
             var IMAGES = [
                 'doughnut',
@@ -28,6 +35,15 @@ angular.module('tcp').component('compliments', {
             this.image = null;
             this.message = null;
 
+            this.hide = function () {
+                this.popover.hide();
+            };
+
+            this.hide_and_stop = function () {
+                this.hide();
+                localStorage.setItem(KEY, '1');
+            };
+
             $interval(function () {
                 this.image = rand(IMAGES);
                 this.message = i18n.get('compliments/' + rand(STRINGS));
@@ -40,6 +56,14 @@ angular.module('tcp').component('compliments', {
              */
             function rand(arr) {
                 return arr[Math.floor(Math.random() * arr.length)];
+            }
+
+            /**
+             * @return {boolean}
+             */
+            function can_show_compliments() {
+                return localStorage.getItem(KEY) &&
+                    localStorage.getItem(KEY) === '1';
             }
         }
     ],
