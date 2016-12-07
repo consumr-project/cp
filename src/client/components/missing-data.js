99177caabd7a0d4edc7929aea009e45e670b0dbb
Marcos Minond <minond.marcos@gmail.com>
2016-12-06 18:57:40 -0700

    triggering compliments on %5 updated companies

--- src/client/components/missing-data.js
+++ src/client/components/missing-data.js
@@ -4,7 +4,7 @@ angular.module('tcp').component('missingData', {
         '<div ng-class="{loading: $ctrl.loading, \'missing-data--active\': $ctrl.selected}">',
             '<p ng-if="$ctrl.allowed" class="missing-data__title" i18n="company/we_need_data"></p>',
             '<message ng-if="!$ctrl.allowed" type="error" i18n="admin/error_login_to_do_this"></message>',
-            '<compliments></compliments>',
+            '<compliments api="$ctrl.compliment"></compliments>',
             '<div class="missing-data__loading loading__only" i18n="common/loading"></div>',
             '<div ng-repeat="company in $ctrl.companies">',
                 '<div class="missing-data__item" ',
@@ -22,13 +22,19 @@ angular.module('tcp').component('missingData', {
         'Services',
         'Session',
         'lodash',
-        function (Services, Session, lodash) {
+        '$timeout',
+        function (Services, Session, lodash, $timeout) {
             'use strict';
+
+            var done_counter = 0;
 
             this.loading = null;
             this.allowed = null;
             this.selected = null;
             this.companies = [];
+
+            // filled in by compliments component
+            this.compliment = {};
 
             this.init = function () {
                 this.loading = true;
@@ -46,6 +52,12 @@ angular.module('tcp').component('missingData', {
             this.saved = function (company) {
                 this.companies = lodash.without(this.companies, company);
                 this.selected = null;
+
+                $timeout(function () {
+                    if (done_counter++ % 5 === 0) {
+                        this.compliment.show();
+                    }
+                }.bind(this), 150);
             };
 
             this.edit = function (company) {
