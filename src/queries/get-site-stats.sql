e535b3aef87d05842abeccb34dada0032225414f
Marcos Minond <minond.marcos@gmail.com>
2017-01-02 18:45:29 -0700

    site stats endpoint

--- src/queries/get-site-stats.sql
+++ src/queries/get-site-stats.sql
@@ -1,31 +1,31 @@
 with
 
 companies as (
-    select count(*) as companies
+    select count(*)::"numeric" as companies
     from companies
     where companies.deleted_date is null
 ),
 
 users as (
-    select count(*) as users
+    select count(*)::"numeric" as users
     from users
     where users.deleted_date is null
 ),
 
 events as (
-    select count(*) as events
+    select count(*)::"numeric" as events
     from events
     where events.deleted_date is null
 ),
 
 tags as (
-    select count(*) as tags
+    select count(*)::"numeric" as tags
     from tags
     where tags.deleted_date is null
 ),
 
 reviews as (
-    select count(*) as reviews
+    select count(*)::"numeric" as reviews
     from reviews
     where reviews.deleted_date is null
 )
