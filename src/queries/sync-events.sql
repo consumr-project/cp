460e6b60cd0387ee6f56f28540572d155869790a
Marcos Minond <minond.marcos@gmail.com>
2017-03-10 22:40:02 -0700

    event river sync query update

--- src/queries/sync-events.sql
+++ src/queries/sync-events.sql
@@ -1,10 +1,22 @@
-select e.id as __id,
+with source as (
+    -- sort of filter these summaries??
+    select distinct es.event_id, es.summary
+    from event_sources es
+    where es.summary is not null
+)
+
+select distinct e.id as __id,
     e.deleted_date is not null as __deleted,
     e.created_date >= :since as __created,
     e.title as __label,
 
     string_agg(distinct lower(c.name), ', ') as companies,
-    string_agg(distinct lower(t."en-US"), ', ') as tags
+    string_agg(distinct lower(t."en-US"), ', ') as tags,
+
+    max(distinct s.summary) as summary,
+
+    date,
+    logo
 
 from events e
 
@@ -13,6 +25,8 @@ left join tags t on (et.tag_id = t.id)
 
 left join company_events ce on (ce.event_id = e.id)
 left join companies c on (ce.company_id = c.id)
+
+left join source s on (s.event_id = e.id)
 
 where e.updated_date > :since
 
